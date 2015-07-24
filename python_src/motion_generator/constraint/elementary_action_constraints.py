3bb5ecbb9d8338d21dc69758ea3b3097833cd370
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-07-24 19:32:29 +0200

    Moved functions for the constraint_extraction.py to MotionConstraints and ElementaryActionConstraints

--- python_src/motion_generator/constraint/elementary_action_constraints.py
+++ python_src/motion_generator/constraint/elementary_action_constraints.py
@@ -5,10 +5,11 @@ Created on Thu Jul 16 14:42:41 2015
 @author: erhe01
 """
 import numpy as np
-from constraint_extraction import extract_trajectory_constraint,\
-                            create_trajectory_from_constraint,\
-                            extract_keyframe_constraint,\
-                            extract_all_keyframe_constraints
+from constraint_extraction import transform_point_from_cad_to_opengl_cs, \
+                            transform_unconstrained_indices_from_cad_to_opengl_cs
+from splines.parameterized_spline import ParameterizedSpline
+
+TRAJECTORY_DIM = 3 # spline in cartesian space
 
 class ElementaryActionConstraints(object):
     def __init__(self,action_index, motion_constraints):
@@ -40,32 +41,13 @@ class ElementaryActionConstraints(object):
         root_joint_name = self.parent_constraint.morphable_graph.skeleton.root# currently only trajectories on the Hips joint are supported
         self.trajectory, self.unconstrained_indices = self._extract_trajectory_from_constraint_list(self.constraints, root_joint_name)
     
-        keyframe_constraints = extract_all_keyframe_constraints(self.constraints,
+        keyframe_constraints = self._extract_all_keyframe_constraints(self.constraints,
                                                                 morphable_subgraph)
         self.keyframe_constraints = self._reorder_keyframe_constraints_for_motion_primitves(morphable_subgraph,
                                                                                  keyframe_constraints)
 
 
 
-    def _reorder_keyframe_constraints_for_motion_primitves(self, morphable_subgraph, keyframe_constraints):
-         """ Order constraints extracted by extract_all_keyframe_constraints for each state
-         """
-         constraints = {}#dict of lists
-         #iterate over keyframe labels
-         for label in keyframe_constraints.keys():
-            state = morphable_subgraph.annotation_map[label]
-            time_information = morphable_subgraph.motion_primitive_annotations[state][label]
-            constraints[state] = []
-            # iterate over joints constrained at that keyframe
-            for joint_name in keyframe_constraints[label].keys():
-                # iterate over constraints for that joint
-                for c in keyframe_constraints[label][joint_name]:
-                    # create constraint definition usable by the algorithm
-                    # and add it to the list of constraints for that state
-                    constraint_desc = extract_keyframe_constraint(joint_name,c,\
-                                                morphable_subgraph,time_information)
-                    constraints[state].append(constraint_desc)
-         return constraints
 
 
     def _extract_trajectory_from_constraint_list(self, constraint_list, joint_name):
@@ -78,10 +60,10 @@ class ElementaryActionConstraints(object):
         * unconstrained_indices: list of indices
             Lists of indices of degrees of freedom to ignore in the constraint evaluation.
         """
-        trajectory_constraint = extract_trajectory_constraint(constraint_list,joint_name)
+        trajectory_constraint = self._extract_trajectory_constraint(constraint_list,joint_name)
         if  trajectory_constraint is not None:
             #print "found trajectory constraint"
-            return create_trajectory_from_constraint(trajectory_constraint)
+            return self._create_trajectory_from_constraint(trajectory_constraint)
         else:
             return None, None
 
@@ -103,3 +85,176 @@ class ElementaryActionConstraints(object):
         continue_with_the_loop = distance_to_end > arc_length_offset/2 and \
                             travelled_arc_length < self.trajectory.full_arc_length - arc_length_offset
         return not continue_with_the_loop
+
+
+    def _extract_trajectory_constraint(self, constraints, joint_name="Hips"):
+        """Returns a single trajectory constraint definition for joint joint out of a elementary action constraint list
+        """
+        for c in constraints:
+            if joint_name == c["joint"]: 
+                if "trajectoryConstraints" in c.keys():
+                    return c["trajectoryConstraints"]
+        return None
+    
+    
+    def _create_trajectory_from_constraint(self, trajectory_constraint,scale_factor=1.0):
+        """ Create a spline based on a trajectory constraint. 
+            Components containing None are set to 0, but marked as ignored in the unconstrained_indices list.
+            Note all elements in constraints_list must have the same dimensions constrained and unconstrained.
+        
+        Parameters
+        ----------
+        * trajectory_constraint: list of dict
+        \t Defines a list of control points
+        * scale_factor: float
+        \t Scaling applied on the control points
+        
+        Returns
+        -------
+        * trajectory: ParameterizedSpline
+        \t The trajectory defined by the control points from the trajectory_constraint
+        * unconstrained_indices : list
+        \t List of indices of unconstrained dimensions
+        """
+       
+        
+        assert len(trajectory_constraint)>0  and "position" in trajectory_constraint[0].keys()
+               
+        #extract unconstrained dimensions
+        unconstrained_indices = []
+        idx = 0
+        for v in trajectory_constraint[0]["position"]:
+            if v == None:
+                unconstrained_indices.append(idx)
+            idx += 1            
+        unconstrained_indices = transform_unconstrained_indices_from_cad_to_opengl_cs(unconstrained_indices)
+        #create control points by setting constrained dimensions to 0
+        control_points = []
+    
+        for c in trajectory_constraint:
+            point = [ p*scale_factor if p is not None else 0 for p in c["position"] ]# else 0  where the array is None set it to 0
+            point = transform_point_from_cad_to_opengl_cs(point)
+            control_points.append(point)
+        #print "####################################################"
+        #print "control points are: "
+        #print control_points
+        trajectory =  ParameterizedSpline(control_points, TRAJECTORY_DIM)
+        
+        return trajectory, unconstrained_indices
+    
+    
+        
+    def _extract_keyframe_constraint(self, joint_name,constraint,morphable_subgraph,time_information):
+        """ Creates a dict containing all properties stated explicitly or implicitly in the input constraint
+        Parameters
+        ----------
+        * joint_name : string
+          Name of the joint
+        * constraint : dict
+          Read from json input file 
+        * time_information : string
+          Time information corresponding to an annotation read from morphable graph meta information 
+          
+         Returns
+         -------
+         *constraint_desc : dict
+          Contains the keys joint, position, orientation, semanticAnnotation
+        """
+       
+        position = [None, None, None]       
+        orientation = [None, None, None]
+        first_frame = None
+        last_frame = None
+        if "position" in constraint.keys():
+             position = constraint["position"]
+        if "orientation" in constraint.keys():
+            orientation =constraint["orientation"]
+        #check if last or fist frame from annotation
+    
+        position = transform_point_from_cad_to_opengl_cs(position)
+     
+        if time_information == "lastFrame":
+            last_frame = True
+        elif time_information == "firstFrame":
+            first_frame = True
+        if "semanticAnnotation" in constraint.keys():
+            semanticAnnotation = constraint["semanticAnnotation"]
+        else:
+            semanticAnnotation = {}
+            
+        semanticAnnotation["firstFrame"] = first_frame
+        semanticAnnotation["lastFrame"] = last_frame
+        constraint_desc = {"joint":joint_name,"position":position,"orientation":orientation,"semanticAnnotation": semanticAnnotation}
+        return constraint_desc
+        
+        
+    def _constraint_definition_has_label(self, constraint_definition, label):
+        """ Checks if the label is in the semantic annotation dict of a constraint
+        """
+        #print "check######",constraint_definition
+        if "semanticAnnotation" in constraint_definition.keys():
+            annotation = constraint_definition["semanticAnnotation"]
+            #print "semantic Annotation",annotation
+            if label in annotation.keys():
+                return True
+        return False
+            
+    def _extract_keyframe_constraints_for_label(self, constraint_list, label):
+        """ Returns the constraints associated with the given label. Ordered 
+            based on joint names.
+        Returns
+        ------
+        * key_constraints : dict of lists
+        \t contains the list of the constrained joints
+        """
+        key_constraints= {}
+        for c in constraint_list:
+            joint_name = c["joint"]
+            #print "read constraint"
+            #print c
+            if "keyframeConstraints" in c.keys():# there are keyframe constraints
+                key_constraints[joint_name] = []
+                for constraint_definition in c["keyframeConstraints"]:
+                    print "read constraint",constraint_definition
+                    if self._constraint_definition_has_label(constraint_definition,label):
+                        key_constraints[joint_name].append(constraint_definition)
+        return key_constraints
+                    
+    def _extract_all_keyframe_constraints(self, constraint_list,morphable_subgraph):
+        """Orders the keyframe constraint for the labels found in the metainformation of
+           the elementary actions based on labels as keys
+        Returns
+        -------
+        * keyframe_constraints : dict of dict of lists
+          access as keyframe_constraints["label"]["joint"][index]
+        """
+        keyframe_constraints = {}
+        annotations = morphable_subgraph.annotation_map.keys()#["start_contact",]
+        for label in annotations:
+    #        print "extract constraints for annotation",label
+            keyframe_constraints[label] = self._extract_keyframe_constraints_for_label(constraint_list,label)
+            #key_frame_constraints = extract_keyframe_constraints(constraints,annotion)
+        return keyframe_constraints
+    
+    
+
+
+    def _reorder_keyframe_constraints_for_motion_primitves(self, morphable_subgraph, keyframe_constraints):
+         """ Order constraints extracted by _extract_all_keyframe_constraints for each state
+         """
+         constraints = {}#dict of lists
+         #iterate over keyframe labels
+         for label in keyframe_constraints.keys():
+            state = morphable_subgraph.annotation_map[label]
+            time_information = morphable_subgraph.motion_primitive_annotations[state][label]
+            constraints[state] = []
+            # iterate over joints constrained at that keyframe
+            for joint_name in keyframe_constraints[label].keys():
+                # iterate over constraints for that joint
+                for c in keyframe_constraints[label][joint_name]:
+                    # create constraint definition usable by the algorithm
+                    # and add it to the list of constraints for that state
+                    constraint_desc = self._extract_keyframe_constraint(joint_name,c,\
+                                                morphable_subgraph,time_information)
+                    constraints[state].append(constraint_desc)
+         return constraints
--- python_src/motion_generator/constraint/motion_constraints.py
+++ python_src/motion_generator/constraint/motion_constraints.py
@@ -6,10 +6,8 @@ Created on Tue Jul 14 15:26:29 2015
 """
 
 from copy import copy
-from constraint_extraction import extract_keyframe_annotations, \
-                                transform_point_from_cad_to_opengl_cs
-
 from elementary_action_constraints import ElementaryActionConstraints
+from constraint_extraction import transform_point_from_cad_to_opengl_cs
 
 class MotionConstraints(object):
     """
@@ -24,7 +22,7 @@ class MotionConstraints(object):
     def __init__(self, mg_input, morphable_graph):
         self.morphable_graph = morphable_graph
         self.elementary_action_list = mg_input["elementaryActions"]
-        self.keyframe_annotations = extract_keyframe_annotations(self.elementary_action_list)
+        self.keyframe_annotations = self._extract_keyframe_annotations(self.elementary_action_list)
         self.start_pose = mg_input["startPose"]
         self._transform_from_left_to_right_handed_cs()
         self.action_index = 0
@@ -55,4 +53,27 @@ class MotionConstraints(object):
         else:
             return None
   
+
+    def _extract_keyframe_annotations(self, elementary_action_list):
+        """
+        Returns
+        ------
+        * keyframe_annotations : a list of dicts
+          Contains for every elementary action a dict that associates of events/actions with certain keyframes
+        """
+        keyframe_annotations = []
+        for entry in elementary_action_list:
+            print  "entry#################",entry
+            if "keyframeAnnotations" in entry.keys():
+                annotations = {}
+           
+                for annotation in entry["keyframeAnnotations"]:
+                    key = annotation["keyframe"]
+                    annotations[key] = annotation
+                keyframe_annotations.append(annotations)
+            else:
+                keyframe_annotations.append({})
+        return keyframe_annotations
   
+
+        
