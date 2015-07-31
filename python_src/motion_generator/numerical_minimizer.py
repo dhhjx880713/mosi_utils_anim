Fixed error in spelling of self._objective_function in NumericalMinimizer

python_src/motion_generator/numerical_minimizer.py
@@ -31,10 +31,10 @@ class NumericalMinimizer(object):
         return
         
     def set_objective_function(self, obj):
-        self.objective_function = obj
+        self._objective_function = obj
         return
         
-    def _set_error_func_parameters(self, motion_primitive_node, motion_primitve_constraints, prev_frames):
+    def _set_objective_function_parameters(self, motion_primitive_node, motion_primitve_constraints, prev_frames):
         self._error_func_params = motion_primitive_node.motion_primitive, motion_primitve_constraints, \
                                    prev_frames, self.optimization_settings["error_scale_factor"], \
                                    self.optimization_settings["quality_scale_factor"]
@@ -60,36 +60,40 @@ class NumericalMinimizer(object):
         * x : np.ndarray
               optimal low dimensional motion parameter vector
         """
-    
         if initial_guess is None:
             s0 = np.ravel(gmm.sample())
         else:
             s0 = initial_guess
-    
-        self._set_error_func_parameters(motion_primitive_node, constraints, prev_frames)
-              
-        if self.verbose: 
-            start = time.clock()
-            print "Start optimization using", self.optimization_settings["method"], self.optimization_settings["max_iterations"]
-    #    jac = error_function_jac(s0, data)
-        try:
-            result = minimize(self._objective_function,
-                              s0,
-                              args = (self._error_func_params,),
-                              method=self.optimization_settings["method"], 
-                              #jac = error_function_jac, 
-                              tol = self.optimization_settings["tolerance"],
-                              options={'maxiter': self.optimization_settings["max_iterations"], 'disp' : self.verbose})
-                             
-                             
-      
-        except ValueError as e:
-            print "Warning:", e.message
+        if self._objective_function is not None:
+        
+        
+            self._set_objective_function_parameters(motion_primitive_node, constraints, prev_frames)
+                  
+            if self.verbose: 
+                start = time.clock()
+                print "Start optimization using", self.optimization_settings["method"], self.optimization_settings["max_iterations"]
+        #    jac = error_function_jac(s0, data)
+            try:
+                result = minimize(self._objective_function,
+                                  s0,
+                                  args = (self._error_func_params,),
+                                  method=self.optimization_settings["method"], 
+                                  #jac = error_function_jac, 
+                                  tol = self.optimization_settings["tolerance"],
+                                  options={'maxiter': self.optimization_settings["max_iterations"], 'disp' : self.verbose})
+                                 
+                                 
+          
+            except ValueError as e:
+                print "Warning:", e.message
+                return s0
+                
+            if self.verbose:
+                print "Finished optimization in ",time.clock()-start,"seconds"
+            return result.x
+        else:
+            print "Error: No objective function set. Return initial guess instead."
             return s0
-            
-        if self.verbose:
-            print "Finished optimization in ",time.clock()-start,"seconds"
-        return result.x    
 
 
 