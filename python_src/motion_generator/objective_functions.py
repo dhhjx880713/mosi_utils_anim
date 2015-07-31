dedac90d88be54f9625df3212ed11e936c819798
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-07-31 15:48:17 +0200

    Cleanup of comments in objective_functions.py

--- python_src/motion_generator/objective_functions.py
+++ python_src/motion_generator/objective_functions.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Jul 31 13:21:08 2015
 
-@author: erhe01
+@author: hadu01, erhe01
 """
 
 import numpy as np
@@ -11,23 +11,32 @@ from scipy.optimize.optimize import approx_fprime
 from . import global_counter_dict
                       
 
-def obj_spatial_error_sum(s,data):
-    """ Note: Time parameters and constraints will be ignored. 
+def obj_spatial_error_sum(s, data):
+    """ Calculates the error of a low dimensional motion vector s 
+    given constraints.
+    Note: Time parameters and constraints will be ignored. 
+
+    Parameters
+    ---------
+    * s : np.ndarray
+        low dimensional motion representation
+    * data : tuple
+        Contains  motion_primitive, motion_primitive_constraints, prev_frames
+        
+    Returns
+    -------
+    * error : float
     """
     s = np.asarray(s)
-    motion_primitive, motion_primitive_constraint, prev_frames = data
+    motion_primitive, motion_primitive_constraints, prev_frames = data
 
-    error_sum = motion_primitive_constraint.evaluate(motion_primitive, s, prev_frames,use_time_parameters=False)
+    error_sum = motion_primitive_constraints.evaluate(motion_primitive, s, prev_frames,use_time_parameters=False)
     global_counter_dict["evaluations"] += 1
     return error_sum
     
-def obj_time_error_sum(s,data):
-    """ TODO
-    """
-    error_sum = 0
-    return error_sum
+
     
-def obj_spatial_error_sum_and_naturalness(s,data):
+def obj_spatial_error_sum_and_naturalness(s, data):
     """ Calculates the error of a low dimensional motion vector s given 
         constraints and the prior knowledge from the statistical model
 
@@ -36,18 +45,14 @@ def obj_spatial_error_sum_and_naturalness(s,data):
     * s : np.ndarray
         low dimensional motion representation
     * data : tuple
-        Contains parameters of get_optimal_parameters_from_optimization
+        Contains motion_primitive, motion_primitive_constraints, prev_frames, error_scale_factor, quality_scale_factor
         
     Returns
     -------
-    the error
+    * error : float
 
     """
     
-#    motion_primitive, gmm,  constraints, quality_scale_factor, \
-#    error_scale_factor, bvh_reader,prev_frames, node_name_map, bounding_boxes,\
-#    start_transformation,epsilon = data  # the pre_frames are quaternion frames
-    #kinematic_error = kinematic_error_func(s,data)
     kinematic_error = obj_spatial_error_sum(s,data[:-2])# ignore the kinematic factor and quality factor
     #print "s-vector",optimize_theta
     error_scale_factor = data[-1]
@@ -58,7 +63,7 @@ def obj_spatial_error_sum_and_naturalness(s,data):
     print "error",error
     return error
     
-def obj_spatial_error_sum_and_naturalness_jac(x0, data):
+def obj_spatial_error_sum_and_naturalness_jac(s, data):
     """ jacobian of error function. It is a combination of analytic solution 
         for motion primitive model and numerical solution for kinematic error
     """
@@ -68,7 +73,7 @@ def obj_spatial_error_sum_and_naturalness_jac(x0, data):
     error_scale_factor = data[-1]
     quality_scale_factor = data[-2]
     
-    tmp = np.reshape(x0, (1, len(x0)))
+    tmp = np.reshape(s, (1, len(s)))
     logLikelihoods = _log_multivariate_normal_density_full(tmp,
                                                            gmm.means_, 
                                                            gmm.covars_)
@@ -78,13 +83,28 @@ def obj_spatial_error_sum_and_naturalness_jac(x0, data):
 
     n_models = len(gmm.weights_)
     for i in xrange(n_models):
-        numerator += np.exp(logLikelihoods[i]) * gmm.weights_[i] * np.dot(np.linalg.inv(gmm.covars_[i]), (x0 - gmm.means_[i]))
+        numerator += np.exp(logLikelihoods[i]) * gmm.weights_[i] * np.dot(np.linalg.inv(gmm.covars_[i]), (s - gmm.means_[i]))
 #    numerator = numerator
-    denominator = np.exp(gmm.score([x0])[0])
+    denominator = np.exp(gmm.score([s])[0])
 #    denominator = motion_primitive.gmm.score([x0])[0]
     logLikelihood_jac = numerator / denominator
-    kinematic_jac = approx_fprime(x0, obj_spatial_error_sum, 1e-7, data[-2:])# ignore the kinematic factor and quality factor
+    kinematic_jac = approx_fprime(s, obj_spatial_error_sum, 1e-7, data[-2:])# ignore the kinematic factor and quality factor
     jac = logLikelihood_jac * quality_scale_factor + kinematic_jac * error_scale_factor
     return jac
 
         
+def obj_time_error_sum(s, data):
+    """ Calculates the error for time constraints for certain keyframes
+    Parameters
+    ---------
+    * s : np.ndarray
+        low dimensional motion representation
+    * data : tuple
+        Contains time_constraints, motion, n_steps
+        
+    Returns
+    -------
+    * error : float
+    """
+    error_sum = 0
+    return error_sum
