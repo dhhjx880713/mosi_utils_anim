b94eff0bc04c52229e0cdcd242f2e259f2995ce0
Han Du <han.du@dfki.de>
2015-08-05 09:30:33 +0200

    Changed: update unittest for motion_segmentation

--- python_src/construction/morphable_model_construction_pipeline.py
+++ python_src/construction/morphable_model_construction_pipeline.py
@@ -15,20 +15,16 @@ from fpca.motion_dimension_reduction import MotionDimensionReduction
 from motion_primitive.statistical_model_trainer import StatisticalModelTrainer
 
 def main():
-    # initialization
     elementary_action = 'walk'
     motion_primitive = 'sidestepLeft'   
     params = ConstructionAlgorithmConfigurationBuilder(elementary_action,
                                                        motion_primitive)
-    # preprocessing
     preprocessor = Preprocessor(params)
     preprocessor.preprocess()
-    # dimension reduction
     dimension_reduction = MotionDimensionReduction(preprocessor.warped_motions,
                                                    preprocessor.bvhreader,
                                                    params)  
     dimension_reduction.gen_data_for_modeling()                                             
-    # statistical modeling
     modelTrainer = StatisticalModelTrainer(dimension_reduction.fdata,
                                            params.save_path)
     modelTrainer.gen_motion_primitive_model()                                       
