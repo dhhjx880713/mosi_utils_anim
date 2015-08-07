47043638b7d6a25e3e6a05333057a0abc6cc715f
Han Du <han.du@dfki.de>
2015-08-07 12:27:14 +0200

    Add: unittest for FPCA spatial data

---
+++ python_src/test/construction/fpca/FPCA_spatial_data_test.py
@@ -0,0 +1,30 @@
+__author__ = 'hadu01'
+import os
+import sys
+import numpy as np
+ROOTDIR = os.sep.join(['..'] * 3)
+import json
+sys.path.append(ROOTDIR + os.sep)
+TESTPATH = ROOTDIR + os.sep + r'construction/fpca'
+TESTLIBPATH = ROOTDIR + os.sep + 'test/'
+sys.path.append(TESTPATH)
+sys.path.append(TESTLIBPATH)
+from FPCA_spatial_data import  FPCASpatialData
+TEST_DATA_PATH = ROOTDIR + os.sep + \
+    r'../test_data/constrction/fpca'
+from libtest import params, pytest_generate_tests
+
+class TestFPCASpatialData(object):
+
+    def setup_class(self):
+        motion_data_file = TEST_DATA_PATH + os.sep + 'test_data_spatial_fpca.json'
+        with open(motion_data_file, 'rb') as infile:
+            motion_data = json.load(infile)
+        self.fpca_spatial_data = FPCASpatialData(motion_data, 7, 0.95)
+
+    param_convert_data_for_fpca = [{'res': (103, 29, 79)}]
+
+    @params(param_convert_data_for_fpca)
+    def test_convert_data_for_fpca(self, res):
+        self.fpca_spatial_data.convert_data_for_fpca()
+        assert  self.fpca_spatial_data.reshaped_data.shape == res
