258ac133878036ba37ec0daf0a1a38767d19321a
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-08-13 19:56:09 +0200

    Changed created a test class in test_cluster_tree.py from the existing functions

--- python_src/test/space_partitioning/test_cluster_tree.py
+++ python_src/test/space_partitioning/test_cluster_tree.py
@@ -4,57 +4,71 @@ Created on Wed Jul 29 12:24:01 2015
 
 @author: erhe01
 """
+
 import sys
 import os
+import pytest
 sys.path.append(os.sep.join([".."]*2))
 import time
 import numpy as np
 from space_partitioning.cluster_tree import ClusterTree
+
 TESTOUTPATH = os.sep.join([".."]*3 + ["test_output"])
+
 
 def distance_objective(x, test_value):
     return np.linalg.norm(np.asarray(x)-np.asarray(test_value))
 
-def run_cluster_hierarchy_construction(N, K, X, path):
-    cluster_tree = ClusterTree(N, K)
+def run_cluster_hierarchy_construction(n_subdivisions, max_level, X, filename):
+    cluster_tree = ClusterTree(n_subdivisions, max_level)
     cluster_tree.construct(X)
-    cluster_tree.save_to_file(path)
+    #cluster_tree.save_to_file(path)
+    cluster_tree.save_to_file_pickle(filename)
     return cluster_tree
 
-def run_search_from_path(N, K, path, test_value, error_margin=0.1):
-    cluster_tree = ClusterTree(N, K)
-    cluster_tree.load_from_file(TESTOUTPATH + os.sep + "tree")
-    run_search(N, K, cluster_tree, test_value, error_margin)
+class TestClusterTree(object):
 
-def run_search(N, K, cluster_tree, test_value, error_margin=0.1):
-    n_candidates = 5
-    approx_distance, approx_result = cluster_tree.find_best_example_excluding_search_candidates(distance_objective, test_value, n_candidates)
-    distance, result = cluster_tree.find_best_example_exhaustive(distance_objective, test_value)
-    print "best result for", test_value
-    print "exhaustive:", result, distance
-    print "approximate:", approx_result, approx_distance
-    assert distance < error_margin
+    def setup_class(self):
+        self.n_samples = 10000
+        self.n_dim = 3
+        self.n_subdivisions = 4
+        self.max_level = 4
+        self.error_margin = 0.1
+        self.n_candidates = 5
+        self.X_shape = (self.n_samples, self.n_dim)
+        self.X = np.random.random(self.X_shape)
+        self.test_value = self.X[0]
+        #print self.X.shape
+        self.filename = TESTOUTPATH + os.sep + "tree.pck"
+        start = time.clock()
+        self.cluster_tree = run_cluster_hierarchy_construction(self.n_subdivisions, self.max_level, self.X, self.filename)
+        print "finished construction in ", time.clock()-start, "seconds"
 
 
-def test_construction_and_search():
-    n_samples = 10000
-    n_dim = 3
-    N = 4
-    K = 4
-    X_shape = (n_samples, n_dim)
-    X = np.random.random(X_shape)
-    test_value = X[0]
-    print X.shape
-    start = time.clock()
-    path = TESTOUTPATH + os.sep + "tree"
-    cluster_tree = run_cluster_hierarchy_construction(N, K, X, path)
-    print "finished construction in ", time.clock()-start, "seconds"
-    start = time.clock()
-    run_search(N, K, cluster_tree, test_value)
-    run_search_from_path(N, K, path, test_value)
-    print "finished search in ", time.clock()-start, "seconds"
-    return
-    
+    def test_search_from_file(self):
+        print "from file"
+        loaded_cluster_tree = ClusterTree(self.n_subdivisions, self.max_level)
+        loaded_cluster_tree.load_from_file(TESTOUTPATH + os.sep + "tree")
+        loaded_cluster_tree.load_from_file_pickle(self.filename)
+        self.run_search(loaded_cluster_tree)
+
+    def test_search_from_memory(self):
+        print "from memory"
+        self.run_search(self.cluster_tree)
+
+    def run_search(self, cluster_tree):
+        start = time.clock()
+        approx_distance, approx_result = cluster_tree.find_best_example_excluding_search_candidates(distance_objective, self.test_value, self.n_candidates)
+        distance, result = cluster_tree.find_best_example_exhaustive(distance_objective, self.test_value)
+        print "best result for", self.test_value
+        print "exhaustive:", result, distance
+        print "approximate:", approx_result, approx_distance
+        assert distance < self.error_margin
+        print "finished search in ", time.clock()-start, "seconds"
+
 
 if __name__ == "__main__":
-    test_construction_and_search()
+    test_class = TestClusterTree()
+    test_class.setup_class()
+    test_class.test_search_from_memory()
+    test_class.test_search_from_file()
