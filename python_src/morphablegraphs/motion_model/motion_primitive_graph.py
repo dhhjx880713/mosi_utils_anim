bdead1cb5fbf1d17c16fbf8a2eb11e0169d3a65d
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-08-21 20:30:05 +0200

    Clenead up motion_primitive_graph.py

--- python_src/morphablegraphs/motion_model/motion_primitive_graph.py
+++ python_src/morphablegraphs/motion_model/motion_primitive_graph.py
@@ -20,30 +20,29 @@ class MotionPrimitiveGraph(object):
         self.node_groups = collections.OrderedDict()
         self.nodes = collections.OrderedDict()
 
-    def generate_random_walk(self, start_subgraph, number_of_steps, use_transition_model=True):
+    def generate_random_walk(self, start_action, number_of_steps, use_transition_model=True):
         """ Generates a random graph walk
             
         Parameters
         ----------
-        * start_subgraph: string
-        \tInitial subgraph.
+        * start_action: string
+            Initial action.
         
         * number_of_steps: integer
-        \tNumber of transitions
+            Number of transitions
         
         * use_transition_model: bool
-        \tSets whether or not the transition model should be used in parameter prediction
+            Sets whether or not the transition model should be used in parameter prediction
         
         Returns
         -------
         *graph_walk: a list of dictionaries
-        \t The graph walk is defined by a list of dictionaries containing entries for "subgraph","state" and "parameters"
+            The graph walk is defined by a list of dictionaries containing entries for "action","motion primitive" and "parameters"
         """
-        assert start_subgraph in self.node_groups.keys()
-        print "generate random graph walk for",start_subgraph
-        current_subgraph = start_subgraph
-        start_state = self.node_groups[current_subgraph].get_random_start_state()
-        return self.node_groups[current_subgraph].generate_random_walk(self.nodes, start_state, number_of_steps, use_transition_model)
+        assert start_action in self.node_groups.keys()
+        print "generate random graph walk for", start_action
+        start_state = self.node_groups[start_action].get_random_start_state()
+        return self.node_groups[start_action].generate_random_walk(self.nodes, start_state, number_of_steps, use_transition_model)
     
     def print_information(self):
         """
@@ -55,8 +54,8 @@ class MotionPrimitiveGraph(object):
                 print "\t"+ str(n)
                 print "\t"+"n canonical frames", self.nodes[n].n_canonical_frames
                 print "\t"+"n latent spatial dimensions", self.nodes[n].s_pca["n_components"]
-                print "\t"+"n latent time dimensions",self.nodes[n].t_pca["n_components"]
-                print "\t"+"n basis spatial ",self.nodes[n].s_pca["n_basis"]
+                print "\t"+"n latent time dimensions", self.nodes[n].t_pca["n_components"]
+                print "\t"+"n basis spatial ", self.nodes[n].s_pca["n_basis"]
                 print "\t"+"n basis time ", self.nodes[n].t_pca["n_basis"]
                 print "\t"+"n clusters", len(self.nodes[n].gaussian_mixture_model.weights_)
                 print "\t"+"average length", self.nodes[n].average_step_length
@@ -67,24 +66,17 @@ class MotionPrimitiveGraph(object):
     def get_random_action_transition(self, motion, action_name):
         """ Get random start state based on edge from previous elementary action if possible
         """
-        next_state = None
+        next_node = None
         if motion.step_count > 0:
             prev_node_key = motion.graph_walk[-1].node_key
       
             if prev_node_key in self.nodes.keys():
-
-               to_key = self.nodes[prev_node_key].generate_random_action_transition(action_name)
-               if to_key is not None:
-                   next_state = to_key
-                   return next_state
-               else:
-                   return None
-               print "generate start from transition of last action", prev_action_name, prev_mp_name, to_key
-           
+                next_node = self.nodes[prev_node_key].generate_random_action_transition(action_name)
+            print "generate start from transition of last action", prev_node_key, next_node
         # if there is no previous elementary action or no action transition
         #  use transition to random start state
-        if next_state == "" or next_state not in self.node_groups[action_name].nodes:
-            print next_state, "not in", action_name
-            next_state = self.node_groups[action_name].get_random_start_state()
-            print "generate random start", next_state
-            return next_state
+        if next_node is None or next_node not in self.node_groups[action_name].nodes:
+            print next_node, "not in", action_name
+            next_node = self.node_groups[action_name].get_random_start_state()
+            print "generate random start", next_node
+        return next_node
