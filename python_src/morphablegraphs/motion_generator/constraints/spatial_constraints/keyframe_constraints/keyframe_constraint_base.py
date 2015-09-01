bab3fcaaf330ac5d3fa7347ddc7e304526ff530b
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-09-01 11:12:16 +0200

    Fixed: added property desired_time to KeyframeConstraintBase

--- python_src/morphablegraphs/motion_generator/constraints/spatial_constraints/keyframe_constraints/keyframe_constraint_base.py
+++ python_src/morphablegraphs/motion_generator/constraints/spatial_constraints/keyframe_constraints/keyframe_constraint_base.py
@@ -11,3 +11,8 @@ class KeyframeConstraintBase(SpatialConstraintBase):
         super(KeyframeConstraintBase, self).__init__(precision, weight_factor)
         self.semantic_annotation = constraint_desc["semanticAnnotation"]
         self.canonical_keyframe = constraint_desc["canonical_keyframe"]
+        if "time" in constraint_desc.keys() and constraint_desc["time"] is not None:
+            print constraint_desc["time"]
+            self.desired_time = float(constraint_desc["time"])
+        else:
+            self.desired_time = None
