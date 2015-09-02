ac63a77ec64347130f254606c84210d183b50624
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-09-02 16:07:38 +0200

    Temporary fix: NumericalMinimizerBuilder.build_time_error_minimizer now always sets the optimization method to BFGS TODO: add separate entries for graph walk and motion primitive optimiziation methods

--- python_src/morphablegraphs/motion_generator/optimization/optimizer_builder.py
+++ python_src/morphablegraphs/motion_generator/optimization/optimizer_builder.py
@@ -1,5 +1,5 @@
 __author__ = 'erhe01'
-
+from copy import copy
 from numerical_minimizer import NumericalMinimizer
 from least_squares import LeastSquares
 from ..objective_functions import obj_spatial_error_residual_vector,\
@@ -23,6 +23,8 @@ class OptimizerBuilder(object):
         return minimizer
 
     def build_time_error_minimizer(self):
-        minimizer = NumericalMinimizer(self.algorithm_settings)
+        algorithm_settings = copy(self.algorithm_settings)
+        algorithm_settings["optimization_settings"]["method"] = "BFGS"
+        minimizer = NumericalMinimizer(algorithm_settings)
         minimizer.set_objective_function(obj_time_error_sum)
         return minimizer
