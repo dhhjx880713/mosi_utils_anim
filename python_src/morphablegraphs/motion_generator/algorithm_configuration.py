d12a96e7a9c25035bec9b53e926e89de3163dfc2
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-09-09 17:10:15 +0200

    Fixed added changes to algorithm_config.py which were forgotten in the last commit

--- python_src/morphablegraphs/motion_generator/algorithm_configuration.py
+++ python_src/morphablegraphs/motion_generator/algorithm_configuration.py
@@ -14,7 +14,7 @@ class AlgorithmConfigurationBuilder(object):
     """
     def __init__(self):
         self.use_constraints = True
-        self.use_optimization = True
+        self.local_optimization_mode = "none"
         self.use_transition_model = False
         self.use_constrained_gmm = False
         self.activate_parameter_check = False
@@ -22,7 +22,9 @@ class AlgorithmConfigurationBuilder(object):
         self.use_global_spatial_optimization = False
         self.n_random_samples = 100
         self.constrained_gmm_settings = dict()
-        self.optimization_settings = dict()
+        self.local_optimization_settings = dict()
+        self.global_spatial_optimization_settings = dict()
+        self.global_time_optimization_settings = dict()
         self.trajectory_following_settings = dict()
         self.smoothing_settings = dict()
         self.activate_cluster_search = True
@@ -46,14 +48,14 @@ class AlgorithmConfigurationBuilder(object):
         self.constrained_gmm_settings["max_bad_samples"] = 200
 
     def set_default_optimization_settings(self):
-        self.optimization_settings = dict()
-        self.optimization_settings["method"] = "Nelder-Mead"
-        self.optimization_settings["start_error_threshold"] = 5.0
-        self.optimization_settings["max_iterations"] = 150
-        self.optimization_settings["quality_scale_factor"] = 0.001
-        self.optimization_settings["error_scale_factor"] = 0.01
-        self.optimization_settings["tolerance"] = 0.01
-        self.optimization_settings["spatial_epsilon"] = 0.0
+        self.local_optimization_settings = dict()
+        self.local_optimization_settings["method"] = "Nelder-Mead"
+        self.local_optimization_settings["start_error_threshold"] = 5.0
+        self.local_optimization_settings["max_iterations"] = 150
+        self.local_optimization_settings["quality_scale_factor"] = 0.001
+        self.local_optimization_settings["error_scale_factor"] = 0.01
+        self.local_optimization_settings["tolerance"] = 0.01
+        self.local_optimization_settings["spatial_epsilon"] = 0.0
 
     def set_default_trajectory_following_settings(self):
         self.trajectory_following_settings = dict()
@@ -73,7 +75,7 @@ class AlgorithmConfigurationBuilder(object):
     def from_json(self, filename):
         temp_algorithm_config = load_json_file(filename)
         self.use_constraints = temp_algorithm_config["use_constraints"]
-        self.use_optimization = temp_algorithm_config["use_optimization"]
+        self.local_optimization_mode = temp_algorithm_config["local_optimization_mode"]
         self.use_transition_model = temp_algorithm_config["use_transition_model"]
         self.use_constrained_gmm = temp_algorithm_config["use_constrained_gmm"]
         self.use_global_time_optimization = temp_algorithm_config["use_global_time_optimization"]
@@ -83,7 +85,9 @@ class AlgorithmConfigurationBuilder(object):
         self.n_random_samples = temp_algorithm_config["n_random_samples"]
         self.constrained_gmm_settings = temp_algorithm_config["constrained_gmm_settings"]
         self.trajectory_following_settings = temp_algorithm_config["trajectory_following_settings"]
-        self.optimization_settings = temp_algorithm_config["optimization_settings"]
+        self.local_optimization_settings = temp_algorithm_config["local_optimization_settings"]
+        self.global_spatial_optimization_settings = temp_algorithm_config["global_spatial_optimization_settings"]
+        self.global_time_optimization_settings = temp_algorithm_config["global_time_optimization_settings"]
         self.activate_cluster_search = temp_algorithm_config["activate_cluster_search"]
         self.n_cluster_search_candidates = temp_algorithm_config["n_cluster_search_candidates"]
         self.debug_max_step = temp_algorithm_config["debug_max_step"]
@@ -91,7 +95,7 @@ class AlgorithmConfigurationBuilder(object):
 
     def build(self):
         return {"use_constraints": self.use_constraints,
-                "use_optimization": self.use_optimization,
+                "local_optimization_mode": self.local_optimization_mode,
                 "use_constrained_gmm": self.use_constrained_gmm,
                 "use_transition_model": self.use_transition_model,
                 "use_global_time_optimization": self.use_global_time_optimization,
@@ -99,7 +103,9 @@ class AlgorithmConfigurationBuilder(object):
                 "n_random_samples": self.n_random_samples,
                 "activate_parameter_check": self.activate_parameter_check,
                 "smoothing_settings": self.smoothing_settings,
-                "optimization_settings": self.optimization_settings,
+                "local_optimization_settings": self.local_optimization_settings,
+                "global_spatial_optimization_settings": self.global_spatial_optimization_settings,
+                "global_time_optimization_settings": self.global_time_optimization_settings,
                 "constrained_gmm_settings": self.constrained_gmm_settings,
                 "trajectory_following_settings": self.trajectory_following_settings,
                 "activate_cluster_search": self.activate_cluster_search,
