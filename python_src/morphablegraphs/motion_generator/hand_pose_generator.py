daf10f0f199175c24970924307af9e055093af57
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-10-01 14:26:01 +0200

    Added support for indpendent left and right hand states to HandPoseGenerator

--- python_src/morphablegraphs/motion_generator/hand_pose_generator.py
+++ python_src/morphablegraphs/motion_generator/hand_pose_generator.py
@@ -5,11 +5,11 @@ from ..animation_data.bvh import BVHReader
 from ..animation_data.motion_vector import MotionVector
 from ..animation_data.skeleton import Skeleton
 
+
 class HandPose(object):
     def __init__(self):
         self.pose_vector = []
-        self.indices = []
-        self.joint_names = []
+        self.hand_skeletons = None
 
 
 class HandPoseGenerator(object):
@@ -17,8 +17,8 @@ class HandPoseGenerator(object):
         self.skeleton = skeleton
         self.pose_map = dict()
         self.status_change_map = dict()
-        self.indices = []
-        self.joint_names = []
+        self.left_hand_skeleton = dict()
+        self.right_hand_skeleton = dict()
         self.initialized = False
         return
 
@@ -35,8 +35,8 @@ class HandPoseGenerator(object):
             with open(hand_pose_info_file, "r") as in_file:
                 hand_pose_info = json.load(in_file)
                 self.status_change_map = hand_pose_info["status_change_map"]
-                self.indices = hand_pose_info["indices"]
-                self.joint_names = hand_pose_info["joint_names"]
+                self.right_hand_skeleton = hand_pose_info["right_hand_skeleton"]
+                self.left_hand_skeleton = hand_pose_info["left_hand_skeleton"]
 
             for root, dirs, files in os.walk(hand_pose_directory):
                 for file_name in files:
@@ -47,30 +47,41 @@ class HandPoseGenerator(object):
                         #motion_vector = MotionVector()
                         #motion_vector.from_bvh_reader(bvh_reader)
                         hand_pose = HandPose()
-                        hand_pose.indices = self.indices
-                        hand_pose.joint_names = self.joint_names
+                        hand_pose.hand_skeletons = dict()
+                        hand_pose.hand_skeletons["RightHand"] = self.right_hand_skeleton
+                        hand_pose.hand_skeletons["LeftHand"] = self.left_hand_skeleton
                         hand_pose.pose_vector = skeleton.reference_frame
                         self.pose_map[file_name[:-4]] = hand_pose
             self.initialized = True
         else:
-            print "Error: Could not load hand poses from",hand_pose_directory
+            print "Error: Could not load hand poses from", hand_pose_directory
 
     def generate_hand_poses(self, motion_vector, action_list):
         if self.initialized:
-            status = "standard"
+            right_status = "standard"
+            left_status = "standard"
+
             for i in xrange(motion_vector.n_frames):
                 if i in action_list.keys():
-                    status = self.status_change_map[action_list[i][0]["event"]]#assume there is only one event
-                    print "change hand status to", status
-                self.set_pose_in_frame(status, motion_vector.quat_frames[i])
+                    for event_desc in action_list[i]:
+                        joint_name = event_desc["parameters"]["joint"]
+                        if joint_name == "RightHand" or joint_name == "RightToolEndSite":
+                            right_status = self.status_change_map[event_desc["event"]]
+                            print "change right hand status to", right_status
+                        elif joint_name == "LeftHand" or joint_name == "LeftToolEndSite":
+                            left_status = self.status_change_map[event_desc["event"]]
+                            print "change left hand status to", left_status
 
-    def set_pose_in_frame(self, status, pose_vector):
+                self.set_pose_in_frame("RightHand", right_status, motion_vector.quat_frames[i])
+                self.set_pose_in_frame("LeftHand", left_status, motion_vector.quat_frames[i])
+
+    def set_pose_in_frame(self, hand, status, pose_vector):
         """
         Overwrites the parameters in the given pose vector with the hand pose
         :param pose_vector:
         :return:
         """
-        for i in self.pose_map[status].indices:
+        for i in self.pose_map[status].hand_skeletons[hand]["indices"]:
             frame_index = i*4 + 3 #translation is ignored
-            if frame_index < len(pose_vector):
-                pose_vector[frame_index:frame_index+4] = self.pose_map[status].pose_vector[frame_index:frame_index+4]
+            #print self.pose_map[status].pose_vector[frame_index:frame_index+4]
+            pose_vector[frame_index:frame_index+4] = self.pose_map[status].pose_vector[frame_index:frame_index+4]
