81014bc032178adab3043dc897acdf544a052f02
Erik Herrmann <Erik.Herrmann@dfki.de>
2015-10-30 13:27:17 +0100

    Fixed: deactivated time parameters in MotionPrimitiveSampleGenerator which ignores time parameters Renamed: OptimizerBuilder.build_spatial_error_minimizer to build_spatial_and_naturalness_error_minimizer Added: OptimizerBuilder.build_spatial_error_minimizer that does not optimize naturalness

--- python_src/morphablegraphs/motion_generator/optimization/optimizer_builder.py
+++ python_src/morphablegraphs/motion_generator/optimization/optimizer_builder.py
@@ -2,7 +2,8 @@ __author__ = 'erhe01'
 import copy
 from numerical_minimizer import NumericalMinimizer
 from least_squares import LeastSquares
-from ..objective_functions import obj_spatial_error_residual_vector,\
+from ..objective_functions import obj_spatial_error_sum,\
+                                  obj_spatial_error_residual_vector,\
                                   obj_spatial_error_residual_vector_and_naturalness,\
                                   obj_spatial_error_sum_and_naturalness,\
                                   obj_time_error_sum, \
@@ -15,7 +16,7 @@ class OptimizerBuilder(object):
     def __init__(self, algorithm_settings):
         self.algorithm_settings = algorithm_settings
 
-    def build_spatial_error_minimizer(self):
+    def build_spatial_and_naturalness_error_minimizer(self):
         method = self.algorithm_settings["local_optimization_settings"]["method"]
         if method == "leastsq":
             minimizer = LeastSquares(self.algorithm_settings["local_optimization_settings"])
@@ -23,6 +24,16 @@ class OptimizerBuilder(object):
         else:
             minimizer = NumericalMinimizer(self.algorithm_settings["local_optimization_settings"])
             minimizer.set_objective_function(obj_spatial_error_sum_and_naturalness)
+        return minimizer
+
+    def build_spatial_error_minimizer(self):
+        method = self.algorithm_settings["local_optimization_settings"]["method"]
+        if method == "leastsq":
+            minimizer = LeastSquares(self.algorithm_settings["local_optimization_settings"])
+            minimizer.set_objective_function(obj_spatial_error_residual_vector)#obj_spatial_error_residual_vector
+        else:
+            minimizer = NumericalMinimizer(self.algorithm_settings["local_optimization_settings"])
+            minimizer.set_objective_function(obj_spatial_error_sum)
         return minimizer
 
     def build_time_error_minimizer(self):
