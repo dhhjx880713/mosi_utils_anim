af6de27fcf674fe6aec0fcd52aa14db2a7d402f4
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-02-19 21:33:55 +0100

    Added additional imports to the morphablegraphs package

--- python_src/morphablegraphs/animation_data/__init__.py
+++ python_src/morphablegraphs/animation_data/__init__.py
@@ -3,3 +3,4 @@ ROTATION_TYPE_QUATERNION = 1
 from .motion_vector import MotionVector
 from .motion_editing import align_quaternion_frames, transform_euler_frames, transform_quaternion_frames
 from .bvh import BVHReader, BVHWriter
+from .skeleton import Skeleton
--- python_src/morphablegraphs/motion_generator/__init__.py
+++ python_src/morphablegraphs/motion_generator/__init__.py
@@ -2,3 +2,5 @@
 
 from motion_generator import MotionGenerator
 from algorithm_configuration import AlgorithmConfigurationBuilder
+from inverse_kinematics import InverseKinematics
+from annotated_motion_vector import AnnotatedMotionVector
