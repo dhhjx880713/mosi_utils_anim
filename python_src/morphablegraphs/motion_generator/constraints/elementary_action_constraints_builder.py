5e88d266c139fbe1a31a601aaf9a0162aa7ff8ed
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-03-16 18:55:50 +0100

    Refactoring of ElementaryActionConstraintsBuilder

--- python_src/morphablegraphs/motion_generator/constraints/elementary_action_constraints_builder.py
+++ python_src/morphablegraphs/motion_generator/constraints/elementary_action_constraints_builder.py
@@ -9,8 +9,10 @@ from elementary_action_constraints import ElementaryActionConstraints
 from spatial_constraints import TrajectoryConstraint
 from spatial_constraints import TrajectorySetConstraint
 from . import *
+from ...utilities.log import write_log
 
 REFERENCE_2D_OFFSET = np.array([0, -1])# components correspond to x, z - we assume the motions are initially oriented into that direction
+
 
 class ElementaryActionConstraintsBuilder(object):
     """Generates ElementaryActionConstraints instances based in an MGInputFormatReader.
@@ -25,6 +27,8 @@ class ElementaryActionConstraintsBuilder(object):
     def __init__(self, motion_state_graph, algorithm_config):
         self.mg_input = None
         self.motion_state_graph = motion_state_graph
+        self.default_constraint_weight = 1.0
+        self.constraint_precision = 1.0
         self.set_algorithm_config(algorithm_config)
 
     def set_algorithm_config(self, algorithm_config):
@@ -71,7 +75,7 @@ class ElementaryActionConstraintsBuilder(object):
                 self.start_pose["orientation"] = [0, angle, 0]
             else:
                 self.start_pose["orientation"] = [0, 0, 0]
-            print "set start orientation", self.start_pose["orientation"]
+            write_log("Set start orientation from trajectory to", self.start_pose["orientation"])
         action_constraints.start_pose = self.start_pose
 
     def _add_keyframe_annotations(self, action_constraints, index):
@@ -85,9 +89,8 @@ class ElementaryActionConstraintsBuilder(object):
         action_constraints.contains_user_constraints = False
         if len(action_constraints.keyframe_constraints) > 0:
             action_constraints.contains_user_constraints = self._has_user_defined_constraints(action_constraints)
-
             self._merge_two_hand_constraints(action_constraints)
-        print action_constraints.action_name, action_constraints.keyframe_constraints, action_constraints.contains_user_constraints, "#######################"
+        #print action_constraints.action_name, action_constraints.keyframe_constraints, action_constraints.contains_user_constraints, "#######################"
 
     def _has_user_defined_constraints(self, action_constraints):
         for keyframe_label_constraints in action_constraints.keyframe_constraints.values():
@@ -134,11 +137,11 @@ class ElementaryActionConstraintsBuilder(object):
             time = constraint_list[left_hand_index]["time"]
             semantic_annotation = constraint_list[left_hand_index]["semanticAnnotation"]
             merged_constraint_desc = {"joint": joint_names,
-                           "positions": positions,
-                           "orientations": orientations,
-                           "time": time,
-                           "merged": True,
-                           "semanticAnnotation": semantic_annotation}
+                                       "positions": positions,
+                                       "orientations": orientations,
+                                       "time": time,
+                                       "merged": True,
+                                       "semanticAnnotation": semantic_annotation}
             #print "merged keyframe constraint", merged_constraint_desc
             merged_constraint_list.append(merged_constraint_desc)
             merged_constraint_list += [desc for (index, desc) in enumerate(constraint_list)
@@ -174,9 +177,8 @@ class ElementaryActionConstraintsBuilder(object):
                    joint_trajectories = action_constraints.collision_avoidance_constraints
                    joint_names = [traj.joint_name for traj in joint_trajectories]
 
-            action_constraints.ca_trajectory_set_constraint = TrajectorySetConstraint(joint_trajectories,
-                                                                                    joint_names,
-                                                                                    self.motion_state_graph.skeleton, 1.0, 1.0)
+            action_constraints.ca_trajectory_set_constraint = TrajectorySetConstraint(joint_trajectories, joint_names,
+                                                                                       self.motion_state_graph.skeleton, self.constraint_precision, self.default_constraint_weight)
 
     def _create_trajectory_constraint(self, action_index, joint_name, scale_factor=1.0):
         """ Create a spline based on a trajectory constraint definition read from the input file.
@@ -188,24 +190,18 @@ class ElementaryActionConstraintsBuilder(object):
         * trajectory: ParameterizedSpline
         \t The trajectory defined by the control points from the trajectory_constraint or None if there is no constraint
         """
-        trajectory_constraint = None
-        precision = 1.0
-        control_points, unconstrained_indices, active_region = self.mg_input.get_trajectory_from_constraint_list(action_index, joint_name, scale_factor, self.control_point_distance_threshold)
+        traj_c = None
+        control_points, unconstrained_indices, active_region = self.mg_input.extract_trajectory_desc(action_index, joint_name, scale_factor, self.control_point_distance_threshold)
         if control_points is not None and unconstrained_indices is not None:
-
-            trajectory_constraint = TrajectoryConstraint(joint_name,
-                                                         control_points,
-                                                         self.default_spline_type,
-                                                         0,
-                                                         unconstrained_indices,
-                                                         self.motion_state_graph.skeleton,
-                                                         precision,
-                                                         1.0,
-                                                         self.closest_point_search_accuracy,
-                                                         self.closest_point_search_max_iterations)
+            traj_c = TrajectoryConstraint(joint_name, control_points,
+                                          self.default_spline_type, 0,
+                                          unconstrained_indices,
+                                          self.motion_state_graph.skeleton,
+                                          self.constraint_precision, self.default_constraint_weight,
+                                          self.closest_point_search_accuracy,
+                                          self.closest_point_search_max_iterations)
             if active_region is not None and active_region["start_point"] is not None and active_region["end_point"] is not None:
-                range_start, closest_point = trajectory_constraint.get_absolute_arc_length_of_point(active_region["start_point"])
-                range_end, closest_point = trajectory_constraint.get_absolute_arc_length_of_point(active_region["end_point"])
-                trajectory_constraint.set_active_range(range_start, range_end)
-
-        return trajectory_constraint
+                range_start, closest_point = traj_c.get_absolute_arc_length_of_point(active_region["start_point"])
+                range_end, closest_point = traj_c.get_absolute_arc_length_of_point(active_region["end_point"])
+                traj_c.set_active_range(range_start, range_end)
+        return traj_c
