5436aa77edcc3f118b3d3aab3fb671073108e13d
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-04-08 16:06:04 +0200

    Changed Direction2DConstraint to use angle between direction vectors

--- python_src/morphablegraphs/motion_generator/constraints/spatial_constraints/keyframe_constraints/direction_2d_constraint.py
+++ python_src/morphablegraphs/motion_generator/constraints/spatial_constraints/keyframe_constraints/direction_2d_constraint.py
@@ -9,6 +9,7 @@ import numpy as np
 from .....animation_data.motion_editing import pose_orientation_quat
 from keyframe_constraint_base import KeyframeConstraintBase
 from .. import SPATIAL_CONSTRAINT_TYPE_KEYFRAME_DIR_2D
+from math import acos
 
 
 class Direction2DConstraint(KeyframeConstraintBase):
@@ -20,6 +21,7 @@ class Direction2DConstraint(KeyframeConstraintBase):
         self.target_dir = np.array(
             [self.direction_constraint[0], self.direction_constraint[2]])
         self.target_dir = self.target_dir / np.linalg.norm(self.target_dir)
+        self.target_dir_len = np.linalg.norm(self.target_dir)
 
     def evaluate_motion_sample(self, aligned_quat_frames):
         motion_dir = pose_orientation_quat(aligned_quat_frames[self.canonical_keyframe])
@@ -27,8 +29,9 @@ class Direction2DConstraint(KeyframeConstraintBase):
         # root_points = extract_root_positions(aligned_quat_frames)
         # print root_points
         # motion_dir = get_trajectory_dir_from_2d_points(root_points)
-        error = abs(self.target_dir[0] - motion_dir[0]) + \
-            abs(self.target_dir[1] - motion_dir[1])
+        #error = abs(self.target_dir[0] - motion_dir[0]) + \
+        #    abs(self.target_dir[1] - motion_dir[1])
+        error = acos(np.dot(self.target_dir, motion_dir)/ (self.target_dir_len * np.linalg.norm(motion_dir)))
         # print "################################################"
         # print "target direction: ", self.target_dir
         # print "motion dir: ", motion_dir
