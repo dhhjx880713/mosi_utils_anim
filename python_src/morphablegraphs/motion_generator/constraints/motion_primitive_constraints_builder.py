b57d71fec4c0f0fdcc7faab6b6a1af91306aaa55
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-04-15 16:20:52 +0200

    Removed unused assignment in MotionPrimitiveConstraintsBuilder.set_status

--- python_src/morphablegraphs/motion_generator/constraints/motion_primitive_constraints_builder.py
+++ python_src/morphablegraphs/motion_generator/constraints/motion_primitive_constraints_builder.py
@@ -69,7 +69,6 @@ class MotionPrimitiveConstraintsBuilder(object):
         self.status["motion_primitive_name"] = node_key[1]
         self.status["n_canonical_frames"] = n_canonical_frames
         self.status["last_arc_length"] = last_arc_length # defined in actionstate.transition() based on the closest point on the path
-        self.status["aligned_sample_frames"] = aligned_sample_frames[n_prev_frames:]
 
         if prev_frames is None:
             last_pos = self.action_constraints.start_pose["position"]
