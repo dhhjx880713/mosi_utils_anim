839215c7bfb5a6564c531e26d10943703aab9b03
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-04-18 19:31:00 +0200

    Fixed get_n_canonical_frames_mgrd works now also with the new mgrd temporal spline model

--- python_src/morphablegraphs/motion_model/motion_primitive_wrapper.py
+++ python_src/morphablegraphs/motion_model/motion_primitive_wrapper.py
@@ -119,7 +119,7 @@ class MotionPrimitiveModelWrapper(object):
         return self.motion_primitive.n_canonical_frames
 
     def get_n_canonical_frames_mgrd(self):
-        return self.motion_primitive.time.n_canonical_frames
+        return max(self.motion_primitive.time.knots)#.n_canonical_frames
     get_n_canonical_frames = get_n_canonical_frames_mgrd if has_mgrd else get_n_canonical_frames_legacy
 
     def get_n_spatial_components_legacy(self):
