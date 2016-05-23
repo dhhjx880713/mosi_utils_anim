55c7258bfc97d5ff35c9fb3f0e22a866042d5996
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-05-23 19:24:45 +0200

    Removed print statements

--- python_src/morphablegraphs/animation_data/skeleton.py
+++ python_src/morphablegraphs/animation_data/skeleton.py
@@ -323,7 +323,6 @@ class Skeleton(object):
             return None
 
         def create_mgrd_node(mg_node, parent):
-            #print mg_node.node_name
             mgrd_node = MGRDSkeletonNode(mg_node.node_name, parent, mg_node.offset, mg_node.rotation)
             mgrd_node.fixed = mg_node.fixed
             return mgrd_node
@@ -331,7 +330,6 @@ class Skeleton(object):
         def populate(skeleton, mgrd_node):
             node = skeleton.nodes[mgrd_node.name]
             for child in node.children:
-                print child
                 child_node = create_mgrd_node(child, mgrd_node)
                 mgrd_node.add_child(child_node)
                 populate(skeleton, child_node)
