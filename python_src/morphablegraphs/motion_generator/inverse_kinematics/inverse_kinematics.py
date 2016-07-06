36be6cc2b81de87159a7975937f6dc8ea419d990
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-07-06 19:11:24 +0200

    Commented print statements

--- python_src/morphablegraphs/motion_generator/inverse_kinematics/inverse_kinematics.py
+++ python_src/morphablegraphs/motion_generator/inverse_kinematics/inverse_kinematics.py
@@ -146,7 +146,8 @@ class InverseKinematics(object):
         result = self._run_optimization(obj_inverse_kinematics, initial_guess, data, cons)
         position = self.pose.evaluate_position(target_joint)
         error = np.linalg.norm(position-target_position)
-        write_log("finished optimization in",time.clock()-start, "seconds with error", error) #,result["x"].tolist(), initial_guess.tolist()
+        if self.verbose:
+            write_log("finished optimization in",time.clock()-start, "seconds with error", error) #,result["x"].tolist(), initial_guess.tolist()
         self.pose.set_channel_values(result["x"], free_joints)
         return error
 
@@ -214,7 +215,7 @@ class InverseKinematics(object):
                 print "IK iteration", i, error, delta, self.elementary_action_epsilon
 
     def _modify_motion_vector_using_keyframe_constraint_list(self, motion_vector, constraints):
-        write_log("number of ik keyframe constraints", len(constraints))
+        #write_log("number of ik keyframe constraints", len(constraints))
         error = 0.0
         has_multiple_targets = False
         for keyframe, constraints in constraints.items():
@@ -248,7 +249,7 @@ class InverseKinematics(object):
 
     def _modify_frame_using_keyframe_constraint_range(self, motion_vector, constraint, frame_range):
         error = 0.0
-        print "use constraint on frame range"
+        #print "use constraint on frame range"
         for frame in range(frame_range[0],frame_range[1]+1):
             self.set_pose_from_frame(motion_vector.frames[frame])
             error += self._modify_pose_general(constraint)
@@ -303,7 +304,7 @@ class InverseKinematics(object):
         target_direction = None
         if traj_constraint["constrain_orientation"]:
             target_direction = trajectory.get_direction()
-            print "direction",target_direction
+            #print "direction",target_direction
 
         full_length = n_frames*d
         for idx in xrange(n_frames):
@@ -390,12 +391,11 @@ class InverseKinematics(object):
 
     def _modify_motion_vector_using_ca_constraints(self, motion_vector, ca_constraints):
         error = 0.0
-        print "modify motion vector using ca constraints", len(ca_constraints)
+        #print "modify motion vector using ca constraints", len(ca_constraints)
         for c in ca_constraints:
             start_frame = motion_vector.graph_walk.steps[c.step_idx].start_frame
             end_frame = motion_vector.graph_walk.steps[c.step_idx].end_frame
             keyframe = self._find_corresponding_frame(motion_vector, start_frame, end_frame, c.joint_name, c.position)
-            #print "found keyframe",keyframe,start_frame,end_frame,c.joint_name
             error += self._modify_frame_using_keyframe_constraint(motion_vector, c, keyframe)
         return error
 
