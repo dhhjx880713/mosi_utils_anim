7100e860ab7d70bd485ff21d6f1c05c84ebcb58e
Erik Herrmann <Erik.Herrmann@dfki.de>
2016-10-28 20:32:56 +0200

    Added more class to the export of the motion_generator package

--- python_src/morphablegraphs/motion_generator/__init__.py
+++ python_src/morphablegraphs/motion_generator/__init__.py
@@ -4,3 +4,6 @@ from motion_generator import MotionGenerator
 from algorithm_configuration import AlgorithmConfigurationBuilder
 from inverse_kinematics import InverseKinematics
 from annotated_motion_vector import AnnotatedMotionVector
+from graph_walk_generator import GraphWalkGenerator
+from graph_walk_optimizer import GraphWalkOptimizer
+from constraints import MGInputFormatReader
