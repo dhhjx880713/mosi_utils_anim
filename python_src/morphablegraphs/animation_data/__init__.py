88813514ef14b159f4354dff1242e4dab1ff49ef
Erik Herrmann <herrmann@cg.uni-saarland.de>
2017-02-28 18:01:41 +0100

    Added missing animation_data package exports

--- python_src/morphablegraphs/animation_data/__init__.py
+++ python_src/morphablegraphs/animation_data/__init__.py
@@ -4,3 +4,4 @@ from .motion_vector import MotionVector
 from .motion_editing import align_quaternion_frames, transform_euler_frames, transform_quaternion_frames
 from .bvh import BVHReader, BVHWriter
 from .skeleton import Skeleton
+from skeleton_node import SKELETON_NODE_TYPE_ROOT, SKELETON_NODE_TYPE_JOINT, SKELETON_NODE_TYPE_END_SITE
