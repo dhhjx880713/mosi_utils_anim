e50388fa7ac9cdf2d0b3efa904e1405bf7f18f0b
Erik Herrmann <herrmann@cg.uni-saarland.de>
2017-04-20 20:48:37 +0200

    Added support for FBX export to retarget_bvh.py

--- python_src/retarget_bvh.py
+++ python_src/retarget_bvh.py
@@ -1,28 +1,50 @@
 import numpy as np
 from morphablegraphs.animation_data import Skeleton, MotionVector, BVHReader, BVHWriter
-from morphablegraphs.animation_data.retargeting import get_targets_from_motion, ROCKETBOX_TO_GAME_ENGINE_MAP
+from morphablegraphs.external.transformations import quaternion_from_euler
+from morphablegraphs.animation_data.retargeting import get_targets_from_motion, ROCKETBOX_TO_GAME_ENGINE_MAP, ADDITIONAL_ROTATION_MAP
 from morphablegraphs.animation_data.retargeting import get_new_frames_from_direction_constraints as get_new_frames_using_quaternion
 from morphablegraphs.animation_data.retargeting_euler import get_new_euler_frames_from_direction_constraints as get_new_frames_using_euler
 from morphablegraphs.animation_data.fbx_io import load_skeleton_and_animations_from_fbx, export_motion_vector_to_fbx_file
 
 
-def retarget(src_skeleton, src_motion, target_skeleton, out_file, inv_joint_map=ROCKETBOX_TO_GAME_ENGINE_MAP, additional_rotation_map=None, frame_range=None, scale_factor=1.0, use_optimization=True, use_euler=False):
+def convert_euler_to_quat_frame(skeleton, frame):
+    n_dims = len(skeleton.animated_joints) * 4 + 3
+    quat_frame = np.zeros(n_dims)
+    quat_frame[:3] = frame[:3]
+    target_offset = 3
+    src_offset = 3
+    for idx, node in enumerate(skeleton.animated_joints):
+        e = np.radians(frame[src_offset:src_offset + 3])
+        q = quaternion_from_euler(*e)
+        quat_frame[target_offset:target_offset + 4] = q
+        target_offset += 4
+        src_offset += 3
+    return quat_frame
+
+
+def create_motion_vector_from_euler_frames(skeleton, euler_frames):
+    mv = MotionVector()
+    mv.frame_time = skeleton.frame_time
+    quat_frames = []
+    for e_frame in euler_frames:
+        quat_frames.append(convert_euler_to_quat_frame(skeleton, e_frame))
+    mv.frames = quat_frames
+    return mv
+
+
+def retarget(src_skeleton, src_motion, target_skeleton, inv_joint_map=ROCKETBOX_TO_GAME_ENGINE_MAP, additional_rotation_map=None, frame_range=None, scale_factor=1.0, use_optimization=True, use_euler=False):
     targets = get_targets_from_motion(src_skeleton, src_motion.frames, inv_joint_map, additional_rotation_map=additional_rotation_map)
     if not use_euler:
         new_frames = get_new_frames_using_quaternion(target_skeleton, targets,
                                                      frame_range=frame_range,
                                                      scale_factor=scale_factor,
                                                      use_optimization=use_optimization)
-        mv = MotionVector()
-        mv.frames = new_frames
 
-        mv.export(target_skeleton, ".", out_file, add_time_stamp=False)
     else:
-        new_frames = get_new_frames_using_euler(target_skeleton,
-                                    targets,
-                                    frame_range=frame_range,
-                                    scale_factor=scale_factor)
-        BVHWriter(out_file, target_skeleton, new_frames, target_skeleton.frame_time)
+        new_frames = get_new_frames_using_euler(target_skeleton, targets,
+                                                frame_range=frame_range,
+                                                scale_factor=scale_factor)
+    return new_frames
 
 
 def load_target_skeleton(file_path):
@@ -36,18 +58,33 @@ def load_target_skeleton(file_path):
         skeleton.load_from_bvh(target_bvh, animated_joints, add_tool_joints=False)
     return skeleton
 
+
+def export(skeleton, frames, out_file, use_euler=False, format="bvh"):
+    if not use_euler:
+        mv = MotionVector()
+        mv.frame_time = skeleton.frame_time
+        mv.frames = frames
+    else:
+        mv = create_motion_vector_from_euler_frames(skeleton, frames)
+
+    if format == "bvh":
+        mv.export(target_skeleton, ".", out_file, add_time_stamp=False)
+    else:
+        export_motion_vector_to_fbx_file(skeleton, mv, out_file+".fbx")
+
 def scale_skeleton(skeleton, scale_factor):
     for node in skeleton.nodes.values():
         node.offset = np.array(node.offset) * scale_factor
 
 if __name__ == "__main__":
 
-    src_file = "export15.bvh"
-    target_file = "game_engine_target.bvh"
-    out_file = "out_euler"
-    frame_range = [0,1]
-    use_optimization = False
-    use_euler = True
+    src_file = "skeleton.bvh"
+    target_file = "game_engine_target.fbx"
+    out_file = "out8"
+    export_format = "fbx"
+    frame_range = [0,50]
+    use_optimization = True
+    use_euler = False
     scale_factor = 10  # is applied on the root translation of the source and the offsets of the skeleton
 
     src_bvh = BVHReader(src_file)
@@ -61,12 +98,13 @@ if __name__ == "__main__":
 
         scale_skeleton(target_skeleton, scale_factor)
 
-        retarget(src_skeleton, src_motion, target_skeleton, out_file,
+        new_frames = retarget(src_skeleton, src_motion, target_skeleton,
                               inv_joint_map=ROCKETBOX_TO_GAME_ENGINE_MAP,
-                              additional_rotation_map=None,
+                              additional_rotation_map=ADDITIONAL_ROTATION_MAP,
                               frame_range=frame_range,
-                              scale_factor=1.0/scale_factor,
+                              scale_factor=1.0,
                               use_optimization=use_optimization,
                               use_euler=use_euler)
+        export(target_skeleton,new_frames,out_file, use_euler, export_format)
     else:
         print "Error: could not read target skeleton", target_file
