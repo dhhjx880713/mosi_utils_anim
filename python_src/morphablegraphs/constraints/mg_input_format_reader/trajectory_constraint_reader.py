19f390d68dc4b7f0d2d9a2af897ed352d6476ab6
Erik Herrmann <herrmann@cg.uni-saarland.de>
2017-05-10 18:30:21 +0200

    Refactored TrajectoryConstraintReader

--- python_src/morphablegraphs/constraints/mg_input_format_reader/trajectory_constraint_reader.py
+++ python_src/morphablegraphs/constraints/mg_input_format_reader/trajectory_constraint_reader.py
@@ -8,28 +8,22 @@ class TrajectoryConstraintReader(object):
         self.activate_coordinate_transform = activate_coordinate_transform
         self.scale_factor = scale_factor
 
-    def _is_active_trajectory_region(self, traj_constraint, index):
-        if "semanticAnnotation" in traj_constraint[index].keys():
-            if "collisionAvoidance" in traj_constraint[index]["semanticAnnotation"].keys():
-                return traj_constraint[index]["semanticAnnotation"]["collisionAvoidance"]
-        return True
-
-    def _extract_trajectory_control_points(self, traj_constraint, distance_threshold=0.0):
-        control_point_list = list()
+    def _filter_control_points(self, control_points, distance_threshold=0.0):
+        filtered_control_points = list()
         active_regions = list()
         previous_point = None
-        n_control_points = len(traj_constraint)
+        n_control_points = len(control_points)
         was_active = False
         last_distance = None
         count = -1
         for idx in xrange(n_control_points):
-            is_active = self._is_active_trajectory_region(traj_constraint, idx)
+            is_active = self._is_active_trajectory_region(control_points, idx)
             if not is_active:
                 was_active = is_active
                 continue
             if not was_active and is_active:
-                active_region = self._init_active_region(traj_constraint)
-                control_point_list.append(list())
+                active_region = self._init_active_region(control_points)
+                filtered_control_points.append(list())
                 active_regions.append(active_region)
                 count += 1
             if count < 0:
@@ -37,21 +31,21 @@ class TrajectoryConstraintReader(object):
             tmp_distance_threshold = distance_threshold
             if active_regions[count] is not None:
                 tmp_distance_threshold = -1
-            result = self._extract_control_point(traj_constraint, n_control_points, idx, previous_point,
-                                                 last_distance, tmp_distance_threshold)
+            result = self._get_control_point_from_list(control_points, n_control_points, idx, previous_point,
+                                                       last_distance, tmp_distance_threshold)
             if result is None:
                 continue
             else:
                 point, last_distance = result
-                n_points = len(control_point_list[count])
+                n_points = len(filtered_control_points[count])
                 if idx == n_control_points - 1:
                     last_added_point_idx = n_points - 1
-                    delta = control_point_list[count][last_added_point_idx] - point
+                    delta = filtered_control_points[count][last_added_point_idx] - point
                     if np.linalg.norm(delta) < distance_threshold:
-                        control_point_list[count][last_added_point_idx] += delta
+                        filtered_control_points[count][last_added_point_idx] += delta
                         write_log(
                             "Warning: shift second to last control point because it is too close to the last control point")
-                control_point_list[count].append(point)
+                filtered_control_points[count].append(point)
 
                 if active_regions[count] is not None:
                     self._update_active_region(active_regions[count], point, is_active)
@@ -60,15 +54,15 @@ class TrajectoryConstraintReader(object):
 
         # handle invalid region specification
         region_points = list()
-        for idx in range(len(control_point_list)):
-            region_points.append(len(control_point_list[idx]))
+        for idx in range(len(filtered_control_points)):
+            region_points.append(len(filtered_control_points[idx]))
             if active_regions[idx] is not None:
-                if len(control_point_list[idx]) < 2:
-                    control_point_list[idx] = None
+                if len(filtered_control_points[idx]) < 2:
+                    filtered_control_points[idx] = None
                 else:
-                    self._end_active_region(active_regions[idx], control_point_list[idx])
+                    self._end_active_region(active_regions[idx], filtered_control_points[idx])
         # print "loaded", len(control_point_list),"active regions with",region_points,"points"
-        return control_point_list, active_regions
+        return filtered_control_points, active_regions
 
     def _init_active_region(self, traj_constraint):
         if "semanticAnnotation" in traj_constraint[0].keys():
@@ -91,14 +85,19 @@ class TrajectoryConstraintReader(object):
         elif not new_active and active_region["start_point"] is not None and active_region["end_point"] is None:
             active_region["end_point"] = point
 
-    def _extract_control_point(self, traj_constraint, n_control_points, index, previous_point, last_distance,
-                               distance_threshold):
-        if "position" not in traj_constraint[index].keys() or traj_constraint[index]["position"] == [None, None,
+    def _is_active_trajectory_region(self, control_points, index):
+        if "semanticAnnotation" in control_points[index].keys():
+            if "collisionAvoidance" in control_points[index]["semanticAnnotation"].keys():
+                return control_points[index]["semanticAnnotation"]["collisionAvoidance"]
+        return True
+    def _get_control_point_from_list(self, control_points, n_control_points, index, previous_point, last_distance,
+                                     distance_threshold):
+        if "position" not in control_points[index].keys() or control_points[index]["position"] == [None, None,
                                                                                                      None]:
             write_log("Warning: skip undefined control point")
             return None
         # set component of the position to 0 where it is is set to None to allow a 3D spline definition
-        point = [p * self.scale_factor if p is not None else 0 for p in traj_constraint[index]["position"]]
+        point = [p * self.scale_factor if p is not None else 0 for p in control_points[index]["position"]]
         point = np.asarray(_transform_point_from_cad_to_opengl_cs(point, self.activate_coordinate_transform))
 
         if previous_point is None or index == n_control_points - 1:
@@ -112,34 +111,21 @@ class TrajectoryConstraintReader(object):
                 return None
             return point, distance
 
-    def extract_trajectory_desc(self, elementary_action_list, action_index, joint_name, distance_threshold=-1):
-        """ Extract the trajectory information from the constraint list
-        Returns:
-        -------
-        * desc : dict
-        \tConstraint definition that contains a list of control points, unconstrained_indices, active_regions and a possible
-        annotation.
-        """
-        desc = dict()
-        desc["control_points_list"] = []
-        desc["orientation_list"] = []
-        desc["unconstrained_indices"] = None
-        desc["active_regions"] = []
-        desc["semantic_annotation"] = None
-        constraint_data = None
-        for c in elementary_action_list[action_index]["constraints"]:
+    def _extract_control_point_list(self, action_desc, joint_name):
+        control_point_list = None
+        for c in action_desc["constraints"]:
             if "joint" in c.keys() and "trajectoryConstraints" in c.keys() and joint_name == c["joint"]:
-                constraint_data = c["trajectoryConstraints"]
+                control_point_list = c["trajectoryConstraints"]
                 break  # there should only be one list per joint and elementary action
-        if constraint_data is not None:
-            for p in constraint_data:
-                if "semanticAnnotation" in p.keys() and not "collisionAvoidance" in p["semanticAnnotation"].keys():
-                    desc["semantic_annotation"] = p["semanticAnnotation"]
-                    break
-            desc["unconstrained_indices"] = self._find_unconstrained_indices(constraint_data)
-            desc["control_points_list"], desc["active_regions"] = self._extract_trajectory_control_points(
-                constraint_data, distance_threshold)
-        return desc
+        return control_point_list
+
+    def _find_semantic_annotation(self, control_points):
+        semantic_annotation = None
+        for p in control_points:
+            if "semanticAnnotation" in p.keys() and not "collisionAvoidance" in p["semanticAnnotation"].keys():
+                semantic_annotation = p["semanticAnnotation"]
+                break
+        return semantic_annotation
 
     def _find_unconstrained_indices(self, trajectory_constraint_data):
         """extract unconstrained dimensions"""
@@ -178,3 +164,28 @@ class TrajectoryConstraintReader(object):
                         break
                 c_index += 1
         return active_region
+
+    def create_trajectory_from_control_points(self, control_points, distance_threshold=-1):
+        desc = dict()
+        desc["control_points_list"] = []
+        desc["orientation_list"] = []
+        desc["active_regions"] = []
+        desc["semantic_annotation"] = self._find_semantic_annotation(control_points)
+        desc["unconstrained_indices"] = self._find_unconstrained_indices(control_points)
+        desc["control_points_list"], desc["active_regions"] = self._filter_control_points(control_points,
+                                                                                          distance_threshold)
+        return desc
+
+    def extract_trajectory_desc(self, elementary_action_list, action_index, joint_name, distance_threshold=-1):
+        """ Extract the trajectory information from the constraint list
+        Returns:
+        -------
+        * desc : dict
+        \tConstraint definition that contains a list of control points, unconstrained_indices, active_regions and a possible
+        annotation.
+        """
+
+        control_points = self._extract_control_point_list(elementary_action_list[action_index], joint_name)
+        if control_points is not None:
+            return self.create_trajectory_from_control_points(control_points, distance_threshold)
+        return {"control_points_list": []}
