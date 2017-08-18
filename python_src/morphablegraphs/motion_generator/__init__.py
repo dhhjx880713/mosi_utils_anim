9e92d9d4f4689c8ede3b431bdc291334a0b1d9d7
Erik Herrmann <herrmann@cg.uni-saarland.de>
2017-08-18 21:55:08 +0200

    Commented out LegacyMotionGenerator and LegacyGraphWalkGenerator import

--- python_src/morphablegraphs/motion_generator/__init__.py
+++ python_src/morphablegraphs/motion_generator/__init__.py
@@ -2,6 +2,6 @@
 from .algorithm_configuration import AlgorithmConfigurationBuilder
 from .annotated_motion_vector import AnnotatedMotionVector
 from .graph_walk_optimizer import GraphWalkOptimizer
-from .legacy.graph_walk_generator import GraphWalkGenerator as LegacyGraphWalkGenerator
-from .legacy.motion_generator import MotionGenerator as LegacyMotionGenerator
+#from .legacy.graph_walk_generator import GraphWalkGenerator as LegacyGraphWalkGenerator
+#from .legacy.motion_generator import MotionGenerator as LegacyMotionGenerator
 from .motion_generator import MotionGenerator
