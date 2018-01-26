8fdc5240b36f4772eea6d18babebc52b3a22c944
Erik Herrmann <herrmann@cg.uni-saarland.de>
2018-01-26 15:45:04 +0100

    Updated animation_player_rest_interface to support retargeting

--- python_src/animation_player_rest_interface.py
+++ python_src/animation_player_rest_interface.py
@@ -8,8 +8,9 @@ import tornado.escape
 import tornado.ioloop
 import tornado.web
 import json
-from .morphablegraphs.motion_generator.annotated_motion_vector import AnnotatedMotionVector
-from .morphablegraphs.animation_data import BVHReader, SkeletonBuilder
+from morphablegraphs.motion_generator.annotated_motion_vector import AnnotatedMotionVector
+from morphablegraphs.animation_data import BVHReader, SkeletonBuilder, SKELETON_MODELS
+from morphablegraphs.animation_data.retargeting import retarget_from_src_to_target
 SERVICE_CONFIG_FILE = "config" + os.sep + "service.config"
 ALGORITHM_CONFIG_FILE = "config" + os.sep + "accuracy_algorithm.config"
 
@@ -51,7 +52,9 @@ class GetMotionHandler(tornado.web.RequestHandler):
         self.write(error_string)
 
     def post(self):
-        result_object = self.application.motion_vector.to_unity_format(scale=10)
+        print("get motion")
+        motion_vector = self.application.get_motion_vector()
+        result_object = motion_vector.to_unity_format(scale=1)
         self.write(json.dumps(result_object))
 
 
@@ -67,13 +70,16 @@ class GetSkeletonHandler(tornado.web.RequestHandler):
         self.application = application
 
     def get(self):
+
         error_string = "GET request not implemented. Use POST instead."
         print(error_string)
         self.write(error_string)
 
     def post(self):
-        joint_name_map = {key: key for key in self.application.skeleton.animated_joints}
-        result_object = self.application.skeleton.to_unity_format(joint_name_map=joint_name_map, scale=10)
+        print("get skeleton")
+        skeleton = self.application.get_skeleton()
+        joint_name_map = {key: key for key in skeleton.animated_joints}
+        result_object = skeleton.to_unity_format(joint_name_map=joint_name_map, scale=10)
         self.write(json.dumps(result_object))
 
 
@@ -81,26 +87,57 @@ class UnityRESTApplication(tornado.web.Application):
     """ Extends the Application class with a MotionGenerator instance and algorithm options.
         This allows access to the data in the MGInputHandler class
     """
-    def __init__(self, bvh_path, handlers=None, default_host="", transforms=None, **settings):
+    def __init__(self, src_path, src_model, target_path=None, target_model=None, handlers=None, default_host="", transforms=None, **settings):
         tornado.web.Application.__init__(self, handlers, default_host, transforms)
-        bvh_reader = BVHReader(bvh_path)
-        animated_joints = list(bvh_reader.get_animated_joints())
-        print(animated_joints)
-        self.skeleton = SkeletonBuilder().load_from_bvh(bvh_reader, animated_joints=animated_joints, add_tool_joints=False)
+        src = BVHReader(src_path)
+        animated_joints = list(src.get_animated_joints())
+        self.src_skeleton = SkeletonBuilder().load_from_bvh(src, animated_joints=animated_joints, add_tool_joints=False)
+        self.src_skeleton.skeleton_model = src_model
+
+        if target_path is not None:
+            target = BVHReader(target_path)
+            animated_joints = list(target.get_animated_joints())
+            self.target_skeleton = SkeletonBuilder().load_from_bvh(target, animated_joints=animated_joints, add_tool_joints=False)
+            self.target_skeleton.skeleton_model = target_model
+        else:
+            self.target_skeleton = None
+
         self.algorithm_config = None
-        self.motion_vector = AnnotatedMotionVector(self.algorithm_config)
-        self.motion_vector.skeleton = self.skeleton
-        self.motion_vector.from_bvh_reader(bvh_reader, filter_joints=False)
+        self.src_path = src_path
+
+    def get_skeleton(self):
+        if self.target_skeleton:
+            return self.target_skeleton
+        else:
+            return self.src_skeleton
+
+    def get_motion_vector(self):
+        src = BVHReader(self.src_path)
+        motion_vector = AnnotatedMotionVector(self.algorithm_config)
+        motion_vector.skeleton = self.src_skeleton
+        motion_vector.from_bvh_reader(src, filter_joints=False)
+        scale_factor = 1.0
+        frame_range = None
+        additional_rotation_map = dict()
+        additional_rotation_map["neck_01"] = [30,0,0]
+        if self.target_skeleton:
+            #print("src", self.src_skeleton.skeleton_model["joints"])
+            #print("target", self.target_skeleton.skeleton_model["joints"], self.target_skeleton.animated_joints)
+            motion_vector.frames = retarget_from_src_to_target(self.src_skeleton, self.target_skeleton, motion_vector.frames, additional_rotation_map=additional_rotation_map, scale_factor=scale_factor, frame_range=frame_range, place_on_ground=True)
+            motion_vector.skeleton = self.target_skeleton
+
+        return motion_vector
 
 
 
 class UnityAnimationPlayerInterface(object):
     """Implements the REST Interface used by the CustomAnimationPlayerUI """
 
-    def __init__(self, bvh_path, port=8888):
-        self.application = UnityRESTApplication(bvh_path,
+    def __init__(self, src_path, src_model, target_path=None, target_model=None, port=8888):
+        self.application = UnityRESTApplication(src_path, src_model, target_path, target_model,
                                              [(r"/get_motion", GetMotionHandler),
-                                              (r"/get_skeleton", GetSkeletonHandler)
+                                              (r"/generate_motion", GetMotionHandler),
+                                               (r"/get_skeleton", GetSkeletonHandler)
                                               ])
         self.port = port
 
@@ -108,13 +145,28 @@ class UnityAnimationPlayerInterface(object):
         """ Start the web server loop
         """
         self.application.listen(self.port)
+        print("start")
         tornado.ioloop.IOLoop.instance().start()
 
 
 def main():
-    bvh_path = "game_engine.bvh"
-    if os.path.isfile(bvh_path):
-        mg_service = UnityAnimationPlayerInterface(bvh_path)
+    src_path = "game_engine.bvh"
+    model_dir = r"E:\projects\model_data"
+    data_dir = model_dir+ os.sep + r"hybrit\2_retargeting"
+    src_path = data_dir + os.sep + r"filtered6\fix-screws-by-hand\17-11-20-Hybrit-VW_fix-screws-by-hand_002_snapPoseSkeleton.bvh"
+    src_path = data_dir + os.sep +  r"walk\beginLeftStance\walk_001_1_beginleftStance_386_429_mirrored_from_beginrightStance.bvh"
+    src_path = data_dir + os.sep + r"walk\beginLeftStance\walk_024_1_beginleftStance_320_369_mirrored_from_beginrightStance.bvh"
+    #src_path = data_dir + os.sep + r"filtered6\fix-screws-by-hand\17-11-20-Hybrit-VW_fix-screws-by-hand_002_snapPoseSkeleton.bvh"
+    #src_path = data_dir + os.sep + r"walk\beginLeftStance\test_motion.bvh"
+
+
+    src_path = model_dir+ os.sep + r"hybrit\1_capturing\walk\beginLeftStance_game_engine_skeleton_smoothed_grounded\walk_001_1_beginleftStance_386_429_mirrored_from_beginrightStance.bvh"
+    src_model = SKELETON_MODELS["game_engine"]
+    target_path = model_dir + os.sep + r"hybrit\custom_target.bvh"
+    target_model = SKELETON_MODELS["custom"]
+    port = 8889
+    if os.path.isfile(src_path):
+        mg_service = UnityAnimationPlayerInterface(src_path, src_model, target_path, target_model, port)
         mg_service.start()
     else:
         print("Error: could not open service or algorithm configuration file")
--- python_src/morphablegraphs/animation_data/__init__.py
+++ python_src/morphablegraphs/animation_data/__init__.py
@@ -4,4 +4,5 @@ from .bvh import BVHReader, BVHWriter
 from .skeleton import Skeleton
 from .skeleton_builder import SkeletonBuilder
 from .skeleton_node import SKELETON_NODE_TYPE_ROOT, SKELETON_NODE_TYPE_JOINT, SKELETON_NODE_TYPE_END_SITE, SkeletonRootNode, SkeletonJointNode, SkeletonEndSiteNode
+from .skeleton_models import SKELETON_MODELS
 from .constants import *
