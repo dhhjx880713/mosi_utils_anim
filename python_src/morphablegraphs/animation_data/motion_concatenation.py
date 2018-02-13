864a4b67413959a09ac34df6c81709b1f6ef0c81
Erik Herrmann <herrmann@cg.uni-saarland.de>
2018-02-13 19:05:56 +0100

    Fixed error in concatenation

--- python_src/morphablegraphs/animation_data/motion_concatenation.py
+++ python_src/morphablegraphs/animation_data/motion_concatenation.py
@@ -165,8 +165,8 @@ def get_node_aligning_2d_transform(skeleton, node_name, prev_frames, new_frames,
     m = quaternion_matrix(q)
 
     first_frame_pos = [new_frames[0][0], 0, new_frames[0][2],1.0]
-    rotated_first_frame_pos = np.dot(m, first_frame_pos)[:3]
-    delta = prev_frames[-1][:3] - rotated_first_frame_pos
+    #rotated_first_frame_pos = np.dot(m, first_frame_pos)[:3]
+    delta = prev_frames[-1][:3] - first_frame_pos[:3]
     m[0, 3] = delta[0]
     m[2, 3] = delta[2]
     return m
