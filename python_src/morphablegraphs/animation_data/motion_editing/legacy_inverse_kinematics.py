592fe6a61515f5fee6ec76cc422eed595fe326f1
Erik Herrmann <Erik.Herrmann@dfki.de>
2018-05-17 21:01:44 +0200

    Refactoring of legacy inverse kinematics

--- python_src/morphablegraphs/animation_data/motion_editing/legacy_inverse_kinematics.py
+++ python_src/morphablegraphs/animation_data/motion_editing/legacy_inverse_kinematics.py
@@ -174,12 +174,12 @@ class LegacyInverseKinematics(object):
     def modify_motion_vector(self, motion_vector):
         for idx, elementary_action_ik_constraints in enumerate(motion_vector.ik_constraints):
             write_message_to_log("Apply IK to elementary action " + str(idx), LOG_MODE_DEBUG)
-            self._optimize_elementary_action_ik_constraints(motion_vector, elementary_action_ik_constraints)
+            self._edit_action(motion_vector.frames, elementary_action_ik_constraints)
 
         if self.adapt_hands_during_both_hand_carry:
             self.adapt_hands_during_carry(motion_vector)
 
-    def _optimize_elementary_action_ik_constraints(self, motion_vector, elementary_action_ik_constraints):
+    def _edit_action(self, frames, ik_constraints):
         i = 0
         last_error = None
         keep_running = True
@@ -187,16 +187,13 @@ class LegacyInverseKinematics(object):
         # modify individual keyframes based on constraints
         while keep_running:
             error = 0.0
-            if "trajectories" in list(elementary_action_ik_constraints.keys()):
-                constraints = elementary_action_ik_constraints["trajectories"]
-                c_error = self._modify_motion_vector_using_trajectory_constraint_list(motion_vector,constraints)
+            if "trajectories" in list(ik_constraints.keys()):
+                constraints = ik_constraints["trajectories"]
+                c_error = self._modify_motion_vector_using_trajectory_constraint_list(frames,constraints)
                 error += c_error * trajectory_weights
-            if "collision_avoidance" in list(elementary_action_ik_constraints.keys()):
-                constraints = elementary_action_ik_constraints["collision_avoidance"]
-                error += self._modify_motion_vector_using_ca_constraints(motion_vector, constraints)
-            if "keyframes" in list(elementary_action_ik_constraints.keys()):
-                constraints = elementary_action_ik_constraints["keyframes"]
-                error += self._modify_motion_vector_using_keyframe_constraint_list(motion_vector, constraints)
+            if "keyframes" in list(ik_constraints.keys()):
+                constraints = ik_constraints["keyframes"]
+                error += self._edit_using_keyframe_constraint_list(frames, constraints)
             if last_error is not None:
                 delta = abs(last_error - error)
             else:
@@ -204,60 +201,61 @@ class LegacyInverseKinematics(object):
             last_error = error
             i += 1
             keep_running = i < self.elementary_action_max_iterations and delta > self.elementary_action_epsilon
-            write_message_to_log("IK iteration " + str(i) + " "+ str(error) + " "+  str(delta) + " "+  str(self.elementary_action_epsilon),LOG_MODE_DEBUG)
+            write_message_to_log("IK iteration " + str(i) + " " + str(error) + " " + str(delta) + " " + str(self.elementary_action_epsilon), LOG_MODE_DEBUG)
 
-    def _modify_motion_vector_using_keyframe_constraint_list(self, motion_vector, constraints):
+    def _edit_using_keyframe_constraint_list(self, frames, constraints):
         error = 0.0
-        for keyframe, constraints in list(constraints.items()):
+        for frame_idx, constraints in list(constraints.items()):
             if "single" in list(constraints.keys()):
                 for c in constraints["single"]:
                     if c.optimize:
                         if c.frame_range is not None:
-                            error += self._modify_motion_vector_using_keyframe_constraint_range(motion_vector, c, c.frame_range)
+                            error += self._edit_frame_range(frames, c, c.frame_range)
                         else:
-                            error += self._modify_frame_using_keyframe_constraint(motion_vector, c, keyframe)
+                            error += self._edit_frame(frames, c, frame_idx)
                     if self.activate_look_at and c.look_at:
-                        start = keyframe
-                        end = keyframe+1
-                        self._look_at_in_range(motion_vector, c.position, start, end)
+                        start = frame_idx
+                        end = frame_idx+1
+                        self._look_at_in_range(frames, c.position, start, end)
                         print("orientation constraint", c.orientation)
                         if c.orientation is not None and self.optimize_orientation:
-                            self._set_hand_orientation(motion_vector, c.orientation, c.joint_name, keyframe, start, end)
+                            self._set_hand_orientation(frames, c.orientation, c.joint_name, frame_idx, start, end)
         return error
 
-    def _modify_frame_using_keyframe_constraint(self, motion_vector, constraint, keyframe):
-        self.set_pose_from_frame(motion_vector.frames[keyframe])
+    def _edit_frame(self, frames, constraint, keyframe):
+        print("edit frame", keyframe)
+        self.set_pose_from_frame(frames[keyframe])
         error = self._modify_pose_general(constraint)
-        motion_vector.frames[keyframe] = self.pose.get_vector()
+        frames[keyframe] = self.pose.get_vector()
         if self.window > 0:
-            self.interpolate_around_keyframe(motion_vector.frames, constraint.get_joint_names(), keyframe, self.window)
+            self.interpolate_around_frame(frames, constraint.get_joint_names(), keyframe, self.window)
         return error
 
-    def _modify_motion_vector_using_keyframe_constraint_range(self, motion_vector, constraint, frame_range):
+    def _edit_frame_range(self, frames, constraint, frame_range):
         error = 0.0
         for frame in range(frame_range[0],frame_range[1]+1):
-            self.set_pose_from_frame(motion_vector.frames[frame])
+            self.set_pose_from_frame(frames[frame])
             error += self._modify_pose_general(constraint)
-            motion_vector.frames[frame] = self.pose.get_vector()
+            frames[frame] = self.pose.get_vector()
 
-        self._create_transition_for_frame_range(motion_vector.frames,frame_range[0],frame_range[1], self.pose.free_joints_map[constraint.joint_name])
+        self._create_transition_for_frame_range(frames,frame_range[0],frame_range[1], self.pose.free_joints_map[constraint.joint_name])
         return error
 
-    def interpolate_around_keyframe(self, frames, joint_names, keyframe, window):
+    def interpolate_around_frame(self, frames, joint_names, keyframe, window):
         write_message_to_log("Smooth and interpolate" + str(joint_names), LOG_MODE_DEBUG)
         for target_joint_name in joint_names:
             joint_parameter_indices = self._extract_free_parameter_indices(self.pose.free_joints_map[target_joint_name])
             for joint_name in self.pose.free_joints_map[target_joint_name]:
                 smooth_joints_around_transition_using_slerp(frames, joint_parameter_indices[joint_name], keyframe, window)
 
-    def _look_at_in_range(self, motion_vector, position, start, end):
+    def _look_at_in_range(self, frames, position, start, end):
         start = max(0, start)
-        end = min(motion_vector.frames.shape[0], end)
+        end = min(frames.shape[0], end)
         for idx in range(start, end):
-            self.set_pose_from_frame(motion_vector.frames[idx])
+            self.set_pose_from_frame(frames[idx])
             self.pose.lookat(position)
-            motion_vector.frames[idx] = self.pose.get_vector()
-        self._create_transition_for_frame_range(motion_vector.frames, start, end-1, [self.pose.head_joint])
+            frames[idx] = self.pose.get_vector()
+        self._create_transition_for_frame_range(frames, start, end-1, [self.pose.head_joint])
 
     def _create_transition_for_frame_range(self, frames, start, end, target_joints):
         for target_joint in target_joints:
@@ -267,22 +265,22 @@ class LegacyInverseKinematics(object):
             create_transition_using_slerp(frames, transition_start, start, joint_parameter_indices)
             create_transition_using_slerp(frames, end, transition_end, joint_parameter_indices)
 
-    def _modify_motion_vector_using_trajectory_constraint_list(self, motion_vector, constraints):
+    def _modify_motion_vector_using_trajectory_constraint_list(self, frames, constraints):
         error = 0.0
         for c in constraints:
             if c["fixed_range"]:
-                error += self._modify_motion_vector_using_trajectory_constraint(motion_vector, c)
+                error += self._modify_motion_vector_using_trajectory_constraint(frames, c)
             else:
-                error += self._modify_motion_vector_using_trajectory_constraint_search_start(motion_vector, c)
+                error += self._modify_motion_vector_using_trajectory_constraint_search_start(frames, c)
         return error
 
-    def _modify_motion_vector_using_trajectory_constraint(self, motion_vector, traj_constraint):
+    def _modify_motion_vector_using_trajectory_constraint(self, frames, traj_constraint):
         error_sum = 0.0
         d = traj_constraint["delta"]
         trajectory = traj_constraint["trajectory"]
         start_idx = traj_constraint["start_frame"]
         end_idx = traj_constraint["end_frame"]-1
-        end_idx = min(len(motion_vector.frames)-1,end_idx)
+        end_idx = min(len(frames)-1,end_idx)
         n_frames = end_idx-start_idx + 1
         target_direction = None
         if traj_constraint["constrain_orientation"]:
@@ -295,14 +293,14 @@ class LegacyInverseKinematics(object):
             t = (idx*d)/full_length
             target_position = trajectory.query_point_by_parameter(t)
             keyframe = start_idx+idx
-            self.set_pose_from_frame(motion_vector.frames[keyframe])
+            self.set_pose_from_frame(frames[keyframe])
             error = np.inf
             iter_counter = 0
             while error > self.success_threshold and iter_counter < self.max_retries:
                 error = self._modify_pose(traj_constraint["joint_name"], target_position, target_direction)
                 iter_counter += 1
             error_sum += error
-            motion_vector.frames[keyframe] = self.pose.get_vector()
+            frames[keyframe] = self.pose.get_vector()
         parent_joint = self.pose.get_parent_joint(traj_constraint["joint_name"])
 
         if traj_constraint["joint_name"] in list(self.pose.free_joints_map.keys()):
@@ -310,25 +308,25 @@ class LegacyInverseKinematics(object):
             free_joints = list(set(free_joints+[parent_joint]))
         else:
             free_joints = [parent_joint]
-        self._create_transition_for_frame_range(motion_vector.frames, start_idx, end_idx, free_joints)
+        self._create_transition_for_frame_range(frames, start_idx, end_idx, free_joints)
         return error_sum
 
-    def _modify_motion_vector_using_trajectory_constraint_search_start(self, motion_vector, traj_constraint):
+    def _modify_motion_vector_using_trajectory_constraint_search_start(self, frames, traj_constraint):
         error_sum = 0.0
         trajectory = traj_constraint["trajectory"]
         start_target = trajectory.query_point_by_parameter(0.0)
-        start_idx = self._find_corresponding_frame(motion_vector,
+        start_idx = self._find_corresponding_frame(frames,
                                                    traj_constraint["start_frame"],
                                                    traj_constraint["end_frame"],
                                                    traj_constraint["joint_name"],
                                                    start_target)
         n_frames = traj_constraint["end_frame"]-start_idx + 1
         arc_length = 0.0
-        self.set_pose_from_frame(motion_vector.frames[start_idx])
+        self.set_pose_from_frame(frames[start_idx])
         prev_position = self.pose.evaluate_position(traj_constraint["joint_name"])
         for idx in range(n_frames):
             keyframe = start_idx+idx
-            self.set_pose_from_frame(motion_vector.frames[keyframe])
+            self.set_pose_from_frame(frames[keyframe])
             current_position = self.pose.evaluate_position(traj_constraint["joint_name"])
             arc_length += np.linalg.norm(prev_position-current_position)
             prev_position = current_position
@@ -342,27 +340,27 @@ class LegacyInverseKinematics(object):
                 error = self._modify_pose(traj_constraint["joint_name"], target)
                 iter_counter += 1
             error_sum += error
-            motion_vector.frames[keyframe] = self.pose.get_vector()
+            frames[keyframe] = self.pose.get_vector()
 
-        self._create_transition_for_frame_range(motion_vector.frames, start_idx, keyframe-1, self.pose.free_joints_map[traj_constraint["joint_name"]])
+        self._create_transition_for_frame_range(frames, start_idx, keyframe-1, self.pose.free_joints_map[traj_constraint["joint_name"]])
         return error_sum
 
-    def _find_corresponding_frame_range(self, motion_vector, traj_constraint):
+    def _find_corresponding_frame_range(self, frames, traj_constraint):
         start_idx = traj_constraint["start_frame"]
         end_idx = traj_constraint["end_frame"]
         start_target = traj_constraint["trajectory"].query_point_by_parameter(0.0)
         end_target = traj_constraint["trajectory"].query_point_by_parameter(1.0)
-        start_idx = self._find_corresponding_frame(motion_vector, start_idx, end_idx, traj_constraint["joint_name"], start_target)
-        end_idx = self._find_corresponding_frame(motion_vector, start_idx, end_idx, traj_constraint["joint_name"], end_target)
+        start_idx = self._find_corresponding_frame(frames, start_idx, end_idx, traj_constraint["joint_name"], start_target)
+        end_idx = self._find_corresponding_frame(frames, start_idx, end_idx, traj_constraint["joint_name"], end_target)
         return start_idx, end_idx
 
-    def _find_corresponding_frame(self, motion_vector, start_idx, end_idx, target_joint, target_position):
+    def _find_corresponding_frame(self, frames, start_idx, end_idx, target_joint, target_position):
         closest_start_frame = copy(start_idx)
         min_error = np.inf
         n_frames = end_idx-start_idx
         for idx in range(n_frames):
             keyframe = start_idx+idx
-            self.set_pose_from_frame(motion_vector.frames[keyframe])
+            self.set_pose_from_frame(frames[keyframe])
             position = self.pose.evaluate_position(target_joint)
             error = np.linalg.norm(position-target_position)
             if error <= min_error:
@@ -375,8 +373,8 @@ class LegacyInverseKinematics(object):
         for c in ca_constraints:
             start_frame = motion_vector.graph_walk.steps[c.step_idx].start_frame
             end_frame = motion_vector.graph_walk.steps[c.step_idx].end_frame
-            keyframe = self._find_corresponding_frame(motion_vector, start_frame, end_frame, c.joint_name, c.position)
-            error += self._modify_frame_using_keyframe_constraint(motion_vector, c, keyframe)
+            keyframe = self._find_corresponding_frame(motion_vector.frames, start_frame, end_frame, c.joint_name, c.position)
+            error += self._edit_frame(motion_vector.frames, c, keyframe)
         return error
 
     def _extract_free_parameters(self, free_joints):
@@ -395,14 +393,14 @@ class LegacyInverseKinematics(object):
             indices[joint_name] = list(range(*self.pose.extract_parameters_indices(joint_name)))
         return indices
 
-    def _set_hand_orientation(self, motion_vector, orientation, joint_name, keyframe, start, end):
+    def _set_hand_orientation(self, frames, orientation, joint_name, keyframe, start, end):
         print("set hand orienation", keyframe)
         parent_joint_name = self.pose.get_parent_joint(joint_name)
-        self.set_pose_from_frame(motion_vector.frames[keyframe])
+        self.set_pose_from_frame(frames[keyframe])
         self.pose.set_hand_orientation(parent_joint_name, orientation)
         start = max(0, start)
-        end = min(motion_vector.frames.shape[0], end)
-        self._create_transition_for_frame_range(motion_vector.frames, start, end-1, [parent_joint_name])
+        end = min(frames.shape[0], end)
+        self._create_transition_for_frame_range(frames, start, end-1, [parent_joint_name])
 
     def _find_carry_frame_ranges(self, motion_vector):
         carrying = False
