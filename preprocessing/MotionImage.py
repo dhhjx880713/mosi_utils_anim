b965daa1d0121e5aa32a400c7fb52360acb00c69
noshaba <noshaba@rocketmail.com>
2019-10-09 21:41:02 +0200

    motion image edits

--- preprocessing/MotionImage.py
+++ preprocessing/MotionImage.py
@@ -1,6 +1,6 @@
-from mosi_utils_anim.animation_data import BVHReader, Skeleton, SkeletonBuilder
+from mosi_utils_anim.animation_data import BVHReader, SkeletonBuilder
 from mosi_utils_anim.animation_data.utils import convert_euler_frames_to_cartesian_frames
-from pilutil import imresize
+from .pilutil import imresize
 import numpy as np
 
 class MotionImage():
@@ -25,20 +25,14 @@ class MotionImage():
     def map_motion_to_image(self):
         motion = np.asarray(self.cart_motion)
         n_frames = motion.shape[0]
-        print(motion.shape)
 
         x_arr = motion[:, :, 0].T
         y_arr = motion[:, :, 1].T
         z_arr = motion[:, :, 2].T
-        print('here', x_arr.shape, y_arr.shape, z_arr.shape)
 
-        r = np.multiply((x_arr-x_arr.min()), 255.0/(x_arr.max()-x_arr.min())).tolist()
-        g = np.multiply((y_arr-y_arr.min()), 255.0/(y_arr.max()-y_arr.min())).tolist()
-        b = np.multiply((z_arr-z_arr.min()), 255.0/(z_arr.max()-z_arr.min())).tolist()
-
-        r_arr = np.array([l for l in r for i in range(1)])
-        g_arr = np.array([l for l in g for i in range(1)])
-        b_arr = np.array([l for l in b for i in range(1)])
+        r_arr = (x_arr-x_arr.min()) * 255.0/(x_arr.max()-x_arr.min())
+        g_arr = (y_arr-y_arr.min()) * 255.0/(y_arr.max()-y_arr.min())
+        b_arr = (z_arr-z_arr.min()) * 255.0/(z_arr.max()-z_arr.min())
 
         rgb_arr = np.dstack((r_arr, g_arr, b_arr))
         rgb_motion = imresize(rgb_arr, (self.img_height, n_frames), interp=self.interpolation, mode=None)
