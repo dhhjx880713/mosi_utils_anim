56185e8725a54bd142c803c38dd15b9c69f7541e
Erik Herrmann <erik.herrmann@dfki.de>
2020-01-10 12:05:10 +0100

    Fixed imports after moving functions to a different module

--- animation_data/__init__.py
+++ animation_data/__init__.py
@@ -1,5 +1,6 @@
 from .motion_vector import MotionVector
-from .utils import align_quaternion_frames, transform_euler_frames, transform_quaternion_frames
+from .utils import align_quaternion_frames
+from .motion_concatenation import transform_euler_frames, transform_quaternion_frames
 from .bvh import BVHReader, BVHWriter
 from .skeleton import Skeleton
 from .skeleton_builder import SkeletonBuilder
